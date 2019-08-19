d7c3b0d170e40e4bf5e9007cce652a225089ecf7
benoit128 <laricojohn@gmail.com>
2019-08-19 12:46:46 +0000

    XCore: update prod
    git-svn-id: https://elsa-svn.onera.fr/CASSIOPEE/Trunk/Cassiopee/Apps/Modules@3351 2bbd7fe0-4929-0410-bb6b-e55522daae51

--- XCore/createSrcs.py
+++ XCore/createSrcs.py
@@ -1,23 +1,12 @@
-# Cree le fichier srcs contenant la liste des fichiers a compiler
+# Cree le fichier srcs_paradigma contenant la liste des fichiers a compiler pour paradigma
 import KCore.Dist as Dist
 
-# proper
-cpp_srcs = []
+# paradigma
+cpp_srcs = Dist.getFilesOfExt('XCore/paradigma', ['.c'])
 
-# scotch (manually)
-#cpp_srcs1 = Dist.getFilesOfExt('XCore/scotch', ['.c'])
-cpp_srcs2 = Dist.getFilesOfExt('XCore/paradigma', ['.c'])
-
-f = open('srcs.py', 'w')
-f.write('import KCore.Dist as Dist\n')
-f.write('from KCore.config import *\n')
+f = open('srcs_paradigma.py', 'w')
 
 f.write('#==============================================================================\n')
 f.write('# Fichiers c++\n')
 f.write('#==============================================================================\n')
 f.write('cpp_srcs = %s\n'%str(cpp_srcs))
-f.write('#==============================================================================\n')
-f.write('cpp_srcs1 = %s\n'%str(cpp_srcs1))
-f.write('#==============================================================================\n')
-f.write('cpp_srcs2 = %s\n'%str(cpp_srcs2))
-f.write('#==============================================================================\n')
