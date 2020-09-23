665d1db9325882832e1bc719a2f7d8e6963579a0
Sam Landier <sam.landier@onera.fr>
2020-09-23 15:49:48 +0000

    adding missing files for previous commit
    git-svn-id: https://elsa-svn.onera.fr/CASSIOPEE/Trunk/Cassiopee/Apps/Modules@3691 2bbd7fe0-4929-0410-bb6b-e55522daae51

---
+++ KCore/KCore/Nuga/include/cell_sensor.hxx
@@ -0,0 +1,74 @@
+/*
+ 
+ 
+ 
+              NUGA 
+ 
+ 
+ 
+ */
+//Authors : Sâm Landier (sam.landier@onera.fr)
+
+#ifndef NUGA_CELL_SENSOR_HXX
+#define NUGA_CELL_SENSOR_HXX
+
+#include "Nuga/include/sensor.hxx"
+
+
+namespace NUGA
+{
+
+template <typename mesh_t>
+class cell_sensor : public sensor<mesh_t, Vector_t<E_Int>>
+{
+  public:
+  
+    using sensor_input_t = Vector_t<E_Int>;
+    using parent_t = sensor<mesh_t, sensor_input_t>;
+    using output_t = typename mesh_t::output_t; //fixme: static assert to add : must be ISO => IntVec
+
+    cell_sensor(mesh_t& mesh, eSmoother smoo_type): parent_t(mesh, nullptr)
+    {
+      if (smoo_type == eSmoother::V1_NEIGH)
+        parent_t::_smoother = new V1_smoother<mesh_t>();
+      else if (smoo_type == eSmoother::SHELL)
+        parent_t::_smoother = new shell_smoother<mesh_t>();
+    }
+
+    virtual E_Int assign_data(const sensor_input_t& data) override;
+    
+    void fill_adap_incr(output_t& adap_incr, bool do_agglo) override ;
+};
+
+/// 
+template <typename mesh_t>
+E_Int cell_sensor<mesh_t>::assign_data(const sensor_input_t& data)
+{
+  parent_t::assign_data(data);
+
+  E_Int nphs = parent_t::_hmesh._ng.PHs.size();
+  parent_t::_data.resize(nphs, 0.); // resize anyway to ensure same size as cells
+
+  return 0;
+}
+
+
+///
+template <typename mesh_t>
+void cell_sensor<mesh_t>::fill_adap_incr(output_t& adap_incr, bool do_agglo)
+{
+  // almost nothing to do, just pass the data as cell_adap_incr
+  adap_incr.face_adap_incr.clear();
+  E_Int nb_faces = parent_t::_hmesh._ng.PGs.size();
+  adap_incr.face_adap_incr.resize(nb_faces, 0);
+
+  adap_incr.cell_adap_incr = parent_t::_data;
+
+  std::cout << "cell adapt incr sz : " << adap_incr.cell_adap_incr.size() << std::endl;
+  E_Int minv = *std::min_element(ALL(adap_incr.cell_adap_incr));
+  E_Int maxv = *std::max_element(ALL(adap_incr.cell_adap_incr));
+  std::cout << "cell adapt incr min/max : " << minv << "/" << maxv << std::endl;
+}
+
+}
+#endif
