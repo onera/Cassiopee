459eab04d8842e63eee1cff1d66802ce51b42bc4
imadhammani <poeter@hotmail.fr>
2023-01-26 10:47:21 +0000

    Converter: fixup correction format OpenFOAM
    git-svn-id: https://elsa-svn.onera.fr/CASSIOPEE/Trunk/Cassiopee/Apps/Modules@4352 2bbd7fe0-4929-0410-bb6b-e55522daae51

---
+++ Converter/test/convFoamPT.py
@@ -0,0 +1,20 @@
+# - convertArrays2File (Foam format) -
+import Converter.PyTree as C
+import Generator.PyTree as G
+
+# Write NGon file
+
+#t = G.cartNGon((0,0,0),(1,1,1),(21,21,2))
+#t = G.close(t)
+#t = C.fillEmptyBCWith(t, 'wall', 'BCWall')
+#t = C.newPyTree(['Base', t])
+
+t = C.convertFile2PyTree("t.cgns")
+
+# Write mesh file
+C.convertPyTree2File(t, "out_foam", "fmt_foam")
+
+# Reread
+b = C.convertFile2PyTree("out_foam", "fmt_foam")
+#print(b)
+C.convertPyTree2File(b, 'foam_ngon.plt')
