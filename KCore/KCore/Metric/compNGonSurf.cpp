7cd00cce218dab28955438634e57d22ef8eb960c
vincentcasseau <hystrath@gmail.com>
2023-10-16 08:59:30 +0000

    KCore/Metric: use of universal NGON fonctions and multithreading in fonctions calculating volume and area
    git-svn-id: https://elsa-svn.onera.fr/CASSIOPEE/Trunk/Cassiopee/Apps/Modules@4720 2bbd7fe0-4929-0410-bb6b-e55522daae51

--- KCore/KCore/Metric/compNGonSurf.cpp
+++ KCore/KCore/Metric/compNGonSurf.cpp
@@ -36,83 +36,91 @@ E_Int K_METRIC::compNGonSurf(
   E_Float* xt, E_Float* yt, E_Float* zt, FldArrayI& cn,
   E_Float* sxp, E_Float* syp, E_Float* szp)
 {
-  // Donnees liees a la connectivite
-  E_Int* cnp = cn.begin(); // pointeur sur la connectivite NGon
-  E_Int nfaces = cnp[0]; // nombre total de faces
-  E_Int sizeFN = cnp[1]; //  taille de la connectivite Face/Noeuds
-  E_Int nelts = cnp[sizeFN+2];  // nombre total d'elements
+  // Acces non universel sur le ptrs
+  E_Int* ngon = cn.getNGon();
+  E_Int* nface = cn.getNFace();
+  E_Int* indPG = cn.getIndPG();
+  E_Int* indPH = cn.getIndPH();
+  
+  // Acces universel nbre d'elements
+  E_Int nelts = cn.getNElts(); // nombre total d elements
 
-  // sommets associes a l'element
-  vector<E_Int> vertices;
-  E_Int nbNodes; // nombre de noeuds pour une face donnee
-  FldArrayI pos; K_CONNECT::getPosElts(cn, pos);
-  FldArrayI posFace(nfaces); // tableau de position des faces dans la connectivite
-  K_CONNECT::getPosFaces(cn, posFace);
   FldArrayI dimElt(nelts); // tableau de la dimension des elements
-  K_CONNECT::getDimElts(cn, posFace, dimElt);
-  E_Int dim; // dimension d'un element
-  E_Float xbe, ybe, zbe; // coordonnees du barycentre d'un element
-  E_Int ind, ind1, ind2; // indices de noeuds
-  E_Float surfnx, surfny, surfnz; // normale a la surface d un triangle
-  E_Float l1x, l1y, l1z, l2x, l2y, l2z; // delta de coordonnees de noeuds
-  // parcours des elements
-  for (E_Int elt = 0; elt < nelts; elt++)
+  K_CONNECT::getDimElts(cn, dimElt);
+
+  E_Int ierr = 0; // error index
+
+  #pragma omp parallel
   {
-    sxp[elt] = 0.; syp[elt] = 0.; szp[elt] = 0.;
-    dim = dimElt[elt]; // dimension de l'element
-    vertices.clear();
-    switch (dim)
+    // sommets associes a l'element
+    vector<E_Int> vertices;
+    E_Int nbNodes; // nombre de noeuds pour une face donnee
+    E_Int dim; // dimension d'un element
+    E_Float xbe, ybe, zbe; // coordonnees du barycentre d'un element
+    E_Int ind, ind1, ind2; // indices de noeuds
+    E_Float surfnx, surfny, surfnz; // normale a la surface d un triangle
+    E_Float l1x, l1y, l1z, l2x, l2y, l2z; // delta de coordonnees de noeuds
+    
+    // parcours des elements
+    #pragma omp for
+    for (E_Int elt = 0; elt < nelts; elt++)
     {
-      case 1: // NGon 1D
-        return 1;
-      case 3:
-        return 1;
-
-      case 2: // NGon 2D
+      sxp[elt] = 0.; syp[elt] = 0.; szp[elt] = 0.;
+      dim = dimElt[elt]; // dimension de l'element
+      vertices.clear();
+      switch (dim)
       {
-        // sommets associes a l'elt dans l'ordre
-        K_CONNECT::getVertexIndices(cn.begin(), posFace.begin(), pos[elt], vertices);
+        case 1: // NGon 1D
+          ierr = 1;
+        case 3:
+          ierr = 1;
 
-        // calcul du barycentre be (xbe, ybe, zbe) de l'element
-        nbNodes = vertices.size();
-        xbe = 0.; ybe = 0.; zbe = 0.;
-        for (E_Int n = 0; n < nbNodes; n++)
+        case 2: // NGon 2D
         {
-          ind = vertices[n]-1;
-          xbe += xt[ind]; ybe += yt[ind]; zbe += zt[ind];
-        }
-        xbe = xbe/nbNodes; ybe = ybe/nbNodes; zbe = zbe/nbNodes;
+          // sommets associes a l'elt dans l'ordre
+          K_CONNECT::getVertexIndices(cn, ngon, nface, indPG, indPH,
+                                      elt, vertices);
 
-        // parcours des faces de l'element elt
-        for (E_Int fa = 0; fa < nbNodes-1; fa++)
-        {
-          ind1 = vertices[fa]-1; ind2 = vertices[fa+1]-1;
+          // calcul du barycentre be (xbe, ybe, zbe) de l'element
+          nbNodes = vertices.size();
+          xbe = 0.; ybe = 0.; zbe = 0.;
+          for (E_Int n = 0; n < nbNodes; n++)
+          {
+            ind = vertices[n]-1;
+            xbe += xt[ind]; ybe += yt[ind]; zbe += zt[ind];
+          }
+          xbe = xbe/nbNodes; ybe = ybe/nbNodes; zbe = zbe/nbNodes;
+
+          // parcours des faces de l'element elt
+          for (E_Int fa = 0; fa < nbNodes-1; fa++)
+          {
+            ind1 = vertices[fa]-1; ind2 = vertices[fa+1]-1;
+            // calcul de la normale au triangle (n, n+1, be)
+            l1x = xt[ind1]-xbe; l1y = yt[ind1]-ybe; l1z = zt[ind1]-zbe;
+            l2x = xt[ind2]-xbe; l2y = yt[ind2]-ybe; l2z = zt[ind2]-zbe;
+            surfnx = l1y*l2z-l1z*l2y;
+            surfny = l1z*l2x-l1x*l2z;
+            surfnz = l1x*l2y-l1y*l2x;
+            sxp[elt] += surfnx; syp[elt] += surfny; szp[elt] += surfnz;
+          }
+          // dernier pour boucler
+          ind1 = vertices[nbNodes-1]-1; ind2 = vertices[0]-1;
           // calcul de la normale au triangle (n, n+1, be)
           l1x = xt[ind1]-xbe; l1y = yt[ind1]-ybe; l1z = zt[ind1]-zbe;
           l2x = xt[ind2]-xbe; l2y = yt[ind2]-ybe; l2z = zt[ind2]-zbe;
           surfnx = l1y*l2z-l1z*l2y;
           surfny = l1z*l2x-l1x*l2z;
           surfnz = l1x*l2y-l1y*l2x;
-          sxp[elt] += surfnx; syp[elt] += surfny; szp[elt] += surfnz;
+          sxp[elt] += surfnx;  syp[elt] += surfny;  szp[elt] += surfnz;
+
+          sxp[elt] = 0.5*sxp[elt]; syp[elt] = 0.5*syp[elt]; szp[elt] = 0.5*szp[elt];
         }
-        // dernier pour boucler
-        ind1 = vertices[nbNodes-1]-1; ind2 = vertices[0]-1;
-        // calcul de la normale au triangle (n, n+1, be)
-        l1x = xt[ind1]-xbe; l1y = yt[ind1]-ybe; l1z = zt[ind1]-zbe;
-        l2x = xt[ind2]-xbe; l2y = yt[ind2]-ybe; l2z = zt[ind2]-zbe;
-        surfnx = l1y*l2z-l1z*l2y;
-        surfny = l1z*l2x-l1x*l2z;
-        surfnz = l1x*l2y-l1y*l2x;
-        sxp[elt] += surfnx;  syp[elt] += surfny;  szp[elt] += surfnz;
+        break;
 
-        sxp[elt] = 0.5*sxp[elt]; syp[elt] = 0.5*syp[elt]; szp[elt] = 0.5*szp[elt];
+        default:
+          ierr = 1;
       }
-      break;
-
-      default:
-        return 1;
-
     }
   }
-  return 0;
+  return ierr;
 }
