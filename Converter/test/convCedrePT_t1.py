24e88342c589007d1a3bd9029af460a86d611335
benoit128 <laricojohn@gmail.com>
2024-03-20 10:07:20 +0000

    Converter: ajout LOCAL dans les cas tests
    git-svn-id: https://elsa-svn.onera.fr/CASSIOPEE/Trunk/Cassiopee/Apps/Modules@5003 2bbd7fe0-4929-0410-bb6b-e55522daae51

--- Converter/test/convCedrePT_t1.py
+++ Converter/test/convCedrePT_t1.py
@@ -3,15 +3,17 @@ import Converter.PyTree as C
 import Generator.PyTree as G
 import KCore.test as test
 
+LOCAL = test.getLocal()
+
 # Create grid
 a = G.cartNGon((0,0,0), (1,1,1), (10,10,10))
 C._fillEmptyBCWith(a, 'wall', 'BCWall')
 
 # write file
-C.convertPyTree2File(a, "out.d", "fmt_cedre")
+C.convertPyTree2File(a, LOCAL+"/out.d", "fmt_cedre")
 
 # Reread file
-a = C.convertFile2PyTree("out.d", "fmt_cedre")
+a = C.convertFile2PyTree(LOCAL+"/out.d", "fmt_cedre")
 
 #C.convertPyTree2File(a, 'out.cgns')
 test.testT(a, 1)
--- Converter/test/convMeshPT_t1.py
+++ Converter/test/convMeshPT_t1.py
@@ -3,15 +3,17 @@ import Converter.PyTree as C
 import Generator.PyTree as G
 import KCore.test as test
 
+LOCAL = test.getLocal()
+
 # Create grid
 a = G.cartHexa((0,0,0), (1,1,1), (10,10,10))
 #C._fillEmptyBCWith(a, 'wall', 'BCWall')
 
 # write file
-C.convertPyTree2File(a, "out.mesh", "fmt_mesh")
+C.convertPyTree2File(a, LOCAL+"/out.mesh", "fmt_mesh")
 
 # Reread file
-a = C.convertFile2PyTree("out.mesh", "fmt_mesh")
+a = C.convertFile2PyTree(LOCAL+"/out.mesh", "fmt_mesh")
 
 #C.convertPyTree2File(a, 'out.cgns')
 test.testT(a, 1)
