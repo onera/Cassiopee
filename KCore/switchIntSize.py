a035695fe1a2f3438008d577b52801c321fa7da1
vincentcasseau <hystrath@gmail.com>
2024-04-16 11:31:44 +0000

    KCore: python script to switch between i4 and i8
    git-svn-id: https://elsa-svn.onera.fr/CASSIOPEE/Trunk/Cassiopee/Apps/Modules@5057 2bbd7fe0-4929-0410-bb6b-e55522daae51

---
+++ KCore/switchIntSize.py
@@ -0,0 +1,68 @@
+# Switch size of Int in KCore/Dist.py, ie, toggles EDOUBLEINT and GDOUBLEINT
+# Usage: python switchIntSize.py -i=4
+#        python switchIntSize.py --int=8
+import os
+import sys
+import argparse
+
+CASSIOPEE = os.getenv("CASSIOPEE")
+
+# Parse command-line arguments
+def parseArgs():
+  def _checkInt(i):
+    def _throwError():
+        raise argparse.ArgumentTypeError("Int size must be 4 or 8.")
+        sys.exit()
+    try: i = int(i)
+    except: _throwError()
+    if i in [4, 8]: return i
+    elif i == 32: return 4
+    elif i == 64: return 8
+    else: _throwError()
+      
+  # Create argument parser
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-i", "--int", type=_checkInt,
+                      help="Size of Int: 4 or 8. Default: 4.")
+  # Parse arguments
+  return parser.parse_args()
+    
+# Read KCore/Dist
+def readDist():
+  filename = os.path.join(CASSIOPEE, "Apps/Modules/KCore/Dist.py")
+  if not os.access(filename, os.R_OK):
+    raise Exception("Dist.py can't be read at: {}".format(filename))
+  contents = []
+  with open (filename, 'r') as f:
+    for line in f: contents.append(line)
+  return contents
+
+# Edit Dist
+def editDist(contents, intState):
+  sw1 = True
+  sw2 = True
+  for i, line in enumerate(contents):
+    if sw1 and "EDOUBLEINT =" in line:
+      contents[i] = "EDOUBLEINT = " + intState + '\n'
+      sw1 = False
+    if "GDOUBLEINT =" in line:
+      contents[i] = "GDOUBLEINT = " + intState + '\n'
+      sw2 = False
+    if not (sw1 or sw2): return
+  return
+  
+# Write KCore/Dist
+def writeDist(contents):
+  filename = os.path.join(CASSIOPEE, "Apps/Modules/KCore/Dist.py")
+  if not os.access(filename, os.W_OK):
+    raise Exception("Modified Dist.py can't be written at: {}".format(filename))
+  with open (filename, 'w') as f:
+    for line in contents: f.write(line)
+  return
+  
+if __name__ == '__main__':
+  args = parseArgs()
+  intState = "True" if args.int == 8 else "False"
+  contents = readDist()
+  editDist(contents, intState)
+  writeDist(contents)
