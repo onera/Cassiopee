d68533bf98b82c1d154b92c5508c53ce93f7fd80
vincentcasseau <hystrath@gmail.com>
2024-06-18 19:31:34 +0200

    Converter: cleaning (close) mesh at the end of convertArray2NGon (structured & unstructured). An explicit call to G.close is no longer needed after C.convertArray2NGon now that cleanConnectivity supports NGon v4 and BE/ME. Reverts changes of #fa8839b, #2cdc866, #33da60c, i.e., r4819, r4820, r4822.

--- Cassiopee/Converter/test/convertArray2NGonPT_t1.py
+++ Cassiopee/Converter/test/convertArray2NGonPT_t1.py
@@ -1,10 +1,5 @@
 # - convertArray2NGon(pyTree) -
-import Generator.PyTree as G
 import Converter.PyTree as C
 import KCore.test as test
 
-def convertArray2NGon(a):
-    a = C.convertArray2NGon(a); a = G.close(a, tol=1e-12)
-    return a
-
-test.stdTestT(convertArray2NGon)
+test.stdTestT(C.convertArray2NGon)
--- Cassiopee/Post/test/computeGradLSQPT_m1.py
+++ Cassiopee/Post/test/computeGradLSQPT_m1.py
@@ -13,7 +13,7 @@ def g(x, y, z): return 4.*x + 3.*y + 2.*z
 
 if Cmpi.rank == 0:
     t = G.cartHexa((0.,0.,0.), (1./10.,1./10.,1./10.), (11,11,2))
-    t = C.convertArray2NGon(t); t = G.close(t)
+    t = C.convertArray2NGon(t)
     Internal._adaptNGon32NGon4(t)
     t = C.initVars(t, 'centers:f', f, ['centers:CoordinateX', 'centers:CoordinateY', 'centers:CoordinateZ'])
     t = C.initVars(t, 'centers:g', g, ['centers:CoordinateX', 'centers:CoordinateY', 'centers:CoordinateZ'])
