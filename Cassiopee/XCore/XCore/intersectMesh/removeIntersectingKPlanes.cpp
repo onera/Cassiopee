ffc3c823eb449d37c5eec2adc327d3d73193c00c
imadhammani <poeter@hotmail.fr>
2024-09-05 12:15:46 +0200

    XCore intersectMesh: improved cell quality near intersection zones

--- Cassiopee/XCore/XCore/intersectMesh/removeIntersectingKPlanes.cpp
+++ Cassiopee/XCore/XCore/intersectMesh/removeIntersectingKPlanes.cpp
@@ -24,10 +24,349 @@
 #include "io.h"
 
 static
-PyObject *handle_slave(IMesh *M, Karray& sarray, E_Int min_Kmax);
+void construct_faces(std::vector<std::vector<E_Int>> &faces,
+    const std::vector<E_Int> &apoints, const std::vector<E_Int> &dpoints,
+    E_Int &NF)
+{
+    const E_Int *min_ptr = apoints.data();
+    const E_Int *max_ptr = dpoints.data();
+    E_Int min_range = apoints.size();
+    E_Int max_range = dpoints.size();
+    
+    if (apoints.size() > dpoints.size()) {
+        std::swap(min_ptr, max_ptr);
+        std::swap(min_range, max_range);
+    }
+    
+    for (E_Int l = 0; l < min_range-1; l++) {
+        faces.push_back({min_ptr[l],   max_ptr[l],
+                         max_ptr[l+1], min_ptr[l+1]});
+        NF++;
+    }
+    
+    std::vector<E_Int> last_face;
+    
+    last_face.push_back(min_ptr[min_range-1]);
+    
+    for (E_Int l = min_range-1; l < max_range; l++)
+        last_face.push_back(max_ptr[l]);
+    
+    faces.push_back(last_face);
+    NF++;
+}
 
 static
-E_Int get_kmax(const IMesh *M, const Karray &sarray);
+PyObject *handle_slave(IMesh *M, Karray& sarray)
+{
+    E_Int ni = sarray.ni;
+    E_Int nj = sarray.nj;
+    E_Int nk = sarray.nk;
+    E_Int nij = ni * nj;
+
+    E_Float *Xs = sarray.X;
+    E_Float *Ys = sarray.Y;
+    E_Float *Zs = sarray.Z;
+
+    // Last k-plane outside of M
+    std::vector<E_Int> kmax(nij, -1);
+
+    // Indices of points to be projected
+    std::vector<E_Int> proj_points;
+
+    for (E_Int j = 0; j < nj; j++) {
+        for (E_Int i = 0; i < ni; i++) {
+
+            E_Int base = i + ni*j;
+
+            for (E_Int k = 0; k < nk; k++) {
+
+                E_Int idx = base + nij*k;
+
+                E_Float px = Xs[idx];
+                E_Float py = Ys[idx];
+                E_Float pz = Zs[idx];
+
+                if (M->is_point_inside(px, py, pz)) {
+                    kmax[base] = k-1;
+
+                    // Cache the point to be projected
+                    E_Int proj_id = base + nij*kmax[base];
+                    proj_points.push_back(proj_id);
+                    break;
+                }
+            }
+        }
+    }
+
+    //point_write("proj_points", Xs, Ys, Zs, proj_points);
+
+    E_Int np = ni*nj*nk;
+
+    // Project points onto marray surface
+    std::unordered_map<E_Int, TriangleIntersection> point_hit_table;
+
+    for (E_Int i = 0; i < nij; i++) {
+        E_Int p = proj_points[i];
+        E_Int q = p + nij;
+
+        E_Float px = Xs[p];
+        E_Float py = Ys[p];
+        E_Float pz = Zs[p];
+
+        E_Float dx = Xs[q] - Xs[p];
+        E_Float dy = Ys[q] - Ys[p];
+        E_Float dz = Zs[q] - Zs[p];
+
+        E_Float NORM = sqrt(dx*dx + dy*dy + dz*dz);
+
+        dx /= NORM, dy /= NORM, dz /= NORM;
+
+        TriangleIntersection TI;
+
+        E_Int hit = M->project_point(px, py, pz, dx, dy, dz, TI, i);
+
+        assert(hit);
+
+        TI.pid = p;
+
+        point_hit_table[p] = TI;
+    }
+
+    for (const auto &ploc : point_hit_table) {
+        E_Int fid = ploc.second.face;
+        const auto &pn = M->F[fid];
+        assert(pn.size() == 4);
+        M->faces_to_tri.insert(fid);
+    }
+
+    // Construct the new faces and cells
+
+    std::vector<std::vector<E_Int>> faces;
+    std::vector<std::vector<E_Int>> cells;
+
+    E_Int nf, nc;
+    nf = nc = 0;
+
+    for (E_Int j = 0; j < nj-1; j++) {
+        for (E_Int i = 0; i < ni-1; i++) {
+
+            // Ligne ij
+
+            E_Int bases[4] = {
+                i   + ni * j,
+                i+1 + ni * j,
+                i+1 + ni * (j+1),
+                i   + ni * (j+1)
+            };
+            
+            E_Int done = 0;
+            
+            for (E_Int k = 0; k <= kmax[bases[0]] && !done; k++) {
+
+                E_Int a = bases[0] + k * nij;
+                E_Int b = bases[1] + k * nij;
+                E_Int c = bases[2] + k * nij;
+                E_Int d = bases[3] + k * nij;
+
+                E_Int A, B, C, D;
+                A = B = C = D = -1;
+                
+                // Est-on sur un kmax ?
+
+                if ((k == kmax[bases[0]]) ||
+                    (k == kmax[bases[1]]) ||
+                    (k == kmax[bases[2]]) ||
+                    (k == kmax[bases[3]])) {
+                    done = 1;
+                }
+
+                
+                if (done) {
+
+                    E_Int K;
+
+                    // Accumulate the points
+                    std::vector<E_Int> apoints;
+                    assert(kmax[bases[0]] >= k);
+                    K = k;
+                    while (K <= kmax[bases[0]]) {
+                        apoints.push_back(bases[0] + K*nij);
+                        K++;
+                    }
+                    apoints.push_back(point_hit_table.at(apoints.back()).pid + np);
+
+                    std::vector<E_Int> bpoints;
+                    assert(kmax[bases[1]] >= k);
+                    K = k;
+                    while (K <= kmax[bases[1]]) {
+                        bpoints.push_back(bases[1] + K*nij);
+                        K++;
+                    }
+                    bpoints.push_back(point_hit_table.at(bpoints.back()).pid + np);
+
+                    std::vector<E_Int> cpoints;
+                    assert(kmax[bases[2]] >= k);
+                    K = k;
+                    while (K <= kmax[bases[2]]) {
+                        cpoints.push_back(bases[2] + K*nij);
+                        K++;
+                    }
+                    cpoints.push_back(point_hit_table.at(cpoints.back()).pid + np);
+
+                    std::vector<E_Int> dpoints;
+                    assert(kmax[bases[3]] >= k);
+                    K = k;
+                    while (K <= kmax[bases[3]]) {
+                        dpoints.push_back(bases[3] + K*nij);
+                        K++;
+                    }
+                    dpoints.push_back(point_hit_table.at(dpoints.back()).pid + np);
+
+                    E_Int NF = 0;
+
+                    // BOT
+                    faces.push_back({a, b, c, d});
+                    NF++;
+
+                    // TOP
+                    faces.push_back({apoints.back(),
+                                     bpoints.back(),
+                                     cpoints.back(),
+                                     dpoints.back()});
+                    NF++;
+
+                    // LFT
+                    construct_faces(faces, apoints, dpoints, NF);
+
+                    // RGT
+                    construct_faces(faces, bpoints, cpoints, NF);
+
+                    // FRO
+                    construct_faces(faces, bpoints, apoints, NF);
+
+                    // BCK
+                    construct_faces(faces, cpoints, dpoints, NF);
+
+
+                    // Add the new cell and faces
+
+                    std::vector<E_Int> cell(NF);
+                    for (E_Int l = 0; l < NF; l++) cell[l] = nf+l;
+                    cells.push_back(cell);
+
+                    nf += NF;
+                    nc += 1;
+                }
+                
+                else {
+
+                    A = a + nij;
+                    B = b + nij;
+                    C = c + nij;
+                    D = d + nij;
+                
+                    faces.push_back({a, b, c, d});
+                    faces.push_back({A, B, C, D});
+                    faces.push_back({a, d, D, A});
+                    faces.push_back({b, c, C, B});
+                    faces.push_back({b, a, A, B});
+                    faces.push_back({c, d, D, C});
+
+                    cells.push_back({nf, nf+1, nf+2, nf+3, nf+4, nf+5});
+
+                    nf += 6;
+                    nc += 1;
+
+                }
+
+
+            }
+        }
+    }
+
+    // Renumber points
+
+    std::map<E_Int, E_Int> new_pids;
+    std::map<E_Int, E_Int> old_pids;
+
+    E_Int NP = 0;
+
+    for (auto &pn : faces) {
+        for (auto &p : pn) {
+            auto it = new_pids.find(p);
+            if (it  == new_pids.end()) {
+                new_pids[p] = NP;
+                old_pids[NP] = p;
+                p = NP;
+                NP++;
+            } else {
+                p = it->second; 
+            }
+        }
+    }
+    
+    // Make coordinates
+
+    IMesh new_M;
+
+    new_M.F = faces;
+    new_M.C = cells;
+
+    auto &new_X = new_M.X;
+    auto &new_Y = new_M.Y;
+    auto &new_Z = new_M.Z;
+
+    new_X.resize(NP);
+    new_Y.resize(NP);
+    new_Z.resize(NP);
+
+    for (const auto &pdat : new_pids) {
+        E_Int opid = pdat.first;
+        E_Int npid = pdat.second;
+
+        if (opid < np) {
+            new_X[npid] = Xs[opid];
+            new_Y[npid] = Ys[opid];
+            new_Z[npid] = Zs[opid];
+        } else {
+            const auto &TI = point_hit_table.at(opid - np);
+            new_X[npid] = TI.x;
+            new_Y[npid] = TI.y;
+            new_Z[npid] = TI.z;
+        }
+    }
+
+    new_M.np = NP;
+    new_M.nf = faces.size();
+    new_M.nc = cells.size();
+
+    PyObject *tpl = new_M.export_karray();
+
+    // Tag the projected points
+    npy_intp dims[2];
+    dims[1] = 1;
+    dims[0] = NP;
+    PyArrayObject *tag = (PyArrayObject *)PyArray_SimpleNew(1, dims, NPY_DOUBLE);
+    E_Float *ptag = (E_Float *)PyArray_DATA(tag);
+    memset(ptag, 0, NP*sizeof(E_Float));
+
+    for (const auto &pdat : new_pids) {
+        E_Int opid = pdat.first;
+        E_Int npid = pdat.second;
+
+        if (opid >= np) {
+            ptag[npid] = 1.0;
+        }
+    }
+
+    PyObject *out = PyList_New(0);
+    PyList_Append(out, tpl);
+    PyList_Append(out, (PyObject *)tag);
+    Py_DECREF(tpl);
+    Py_DECREF(tag);
+    
+    return out;
+}
 
 PyObject *K_XCORE::removeIntersectingKPlanes(PyObject *self, PyObject *args)
 {
@@ -72,236 +411,13 @@ PyObject *K_XCORE::removeIntersectingKPlanes(PyObject *self, PyObject *args)
 
     PyObject *slaves_out = PyList_New(0);
 
-    E_Int min_Kmax = std::numeric_limits<E_Int>::max();
-
-    puts("Getting min_Kmax...");
-
-    for (E_Int i = 0; i < nslaves; i++) {
-        E_Int KMAX = get_kmax(M, sarrays[i]);
-        if (KMAX < min_Kmax) min_Kmax = KMAX;
-    }
-
-    //min_Kmax = 10;
-
     for (E_Int i = 0; i < nslaves; i++) {
         printf("Projecting %d / %d\n", i+1, nslaves);
-        PyObject *st = handle_slave(M, sarrays[i], min_Kmax);
+        PyObject *st = handle_slave(M, sarrays[i]);
         PyList_Append(slaves_out, st);
         Py_DECREF(st);
         Karray_free_structured(sarrays[i]);
     }
 
     return slaves_out;
-}
-
-static
-E_Int get_kmax(const IMesh *M, const Karray &sarray)
-{
-    const E_Float *Xs = sarray.X;
-    const E_Float *Ys = sarray.Y;
-    const E_Float *Zs = sarray.Z;
-
-    const E_Int ni = sarray.ni;
-    const E_Int nj = sarray.nj;
-    const E_Int nk = sarray.nk;
-    const E_Int nij = ni * nj;
-    
-
-    /**************************************************************************/
-
-    // Detect at which k does the slave mesh intersect the marray mesh
-    // Get projected points coordinates
-
-    // Max plane index that doesn't intersection with marray bbox (zero-based)
-    E_Int kmax = 0;
-
-    std::vector<E_Int> inside_point;
-
-    for (E_Int k = 0; k < nk; k++, kmax++) {
-        E_Int inside = 0;
-
-        E_Int p = nij*k;
-
-        for (E_Int l = 0; l < nij; l++, p++) {
-
-            if (M->is_point_inside(Xs[p], Ys[p], Zs[p])) {
-                inside_point.push_back(p);
-                inside = 1;
-                break;
-            }
-            
-        }
-
-        if (inside) break;
-    }
-
-    printf("Intersection plane index k: %d / %d\n", kmax, nk);
-
-    return kmax;
-}
-
-static
-PyObject *handle_slave(IMesh *M, Karray& sarray, E_Int kmax)
-{
-    printf("Projecting at kmax index: %d\n", kmax);
-    E_Float *Xs = sarray.X;
-    E_Float *Ys = sarray.Y;
-    E_Float *Zs = sarray.Z;
-
-    E_Int ni = sarray.ni;
-    E_Int nj = sarray.nj;
-    E_Int nk = sarray.nk;
-    E_Int nij = ni * nj;
-    
-
-    /**************************************************************************/
-
-    // points to be projected nij*(kmax-1) .. nij*kmax
-    std::vector<E_Int> proj_points;
-    E_Int ind = (kmax-1)*nij;
-
-    std::vector<E_Int> upper_points;
-
-    for (E_Int l = 0; l < nij; l++) {
-        proj_points.push_back(ind);
-        upper_points.push_back(ind+nij);
-        ind++;
-    }
-
-    //char fname[128] = {};
-    //sprintf(fname, "proj_points%d", idx);
-    //point_write("proj_points", Xs, Ys, Zs, proj_points);
-    //point_write("upper_points", Xs, Ys, Zs, upper_points);
-
-    /**************************************************************************/
-
-    // Project points onto marray surface
-    std::unordered_map<E_Int, TriangleIntersection> point_hit_table;
-
-    for (E_Int i = 0; i < nij; i++) {
-        E_Int p = proj_points[i];
-        E_Int q = p + nij;
-
-        E_Float px = Xs[p];
-        E_Float py = Ys[p];
-        E_Float pz = Zs[p];
-
-        //point_write("bad_point", px, py, pz);
-
-        E_Float dx = Xs[q] - Xs[p];
-        E_Float dy = Ys[q] - Ys[p];
-        E_Float dz = Zs[q] - Zs[p];
-
-        E_Float NORM = sqrt(dx*dx + dy*dy + dz*dz);
-
-        dx /= NORM, dy /= NORM, dz /= NORM;
-
-        TriangleIntersection TI;
-
-        E_Int hit = M->project_point(px, py, pz, dx, dy, dz, TI, i);
-
-        /*
-        if (i == 8) {
-            puts("WRITING BAD EDGE AND POINT");
-            point_write("bad_point", px, py, pz);
-            E_Float qx = px + 100 * dx;
-            E_Float qy = py + 100 * dy;
-            E_Float qz = pz + 100 * dz;
-            edge_write("bad_edge", px, py, pz, qx, qy, qz);
-        }
-        */
-
-        //printf("point %d: t = %f\n", i, TI.t);
-
-        assert(hit);
-
-        point_hit_table[p] = TI;
-    }
-
-    for (const auto &ploc : point_hit_table) {
-        E_Int fid = ploc.second.face;
-        const auto &pn = M->F[fid];
-        assert(pn.size() == 4);
-        M->faces_to_tri.insert(fid);
-    }
-
-
-    /*
-    FILE *fh = fopen("hit_faces", "w");
-    assert(fh);
-    for (const auto hit : point_hit_table) {
-        E_Int pt = hit.first;
-        const auto &TI = hit.second;
-        fprintf(fh, "%d %d\n", pt + nij, TI.face);
-    }
-    fclose(fh);
-    */
-
-    //char fname3[128] = {};
-    //sprintf(fname3, "projection%d", idx);
-    //edge_write(fname3, Xs, Ys, Zs, point_hit_table);
-
-    /*************************************************************************/
-    
-    // planes: 0 .... kmax-1
-    // plane kmax was removed
-    // numbers of planes: kmax + 1
-
-    // Make out cartesian mesh
-    PyObject *tpl;
-    nk = kmax + 1; 
-    tpl = K_ARRAY::buildArray3(3, "x,y,z", ni, nj, nk, 3);
-
-    K_FLD::FldArrayF *f;
-    K_FLD::FldArrayI *c;
-    char *varString, *eltType;
-    K_ARRAY::getFromArray3(tpl, varString, f, ni, nj, nk, c, eltType);
-
-    E_Float* xt = f->begin(1);
-    E_Float* yt = f->begin(2);
-    E_Float* zt = f->begin(3);
-
-    // Copy all the points up to kmax
-    for (E_Int k = 0; k < kmax; k++) {
-        for (E_Int j = 0; j < nj; j++) {
-            for (E_Int i = 0; i < ni; i++) {
-                E_Int ind = i + j*ni + k*nij;
-                xt[ind] = Xs[ind];
-                yt[ind] = Ys[ind];
-                zt[ind] = Zs[ind];
-            }
-        }
-    }
-
-    // Copy the projected points
-    ind = nij*kmax;
-    for (E_Int i = 0; i < nij; i++) {
-        E_Int p = proj_points[i];
-        auto EH = point_hit_table[p];
-        E_Float x = EH.x;
-        E_Float y = EH.y;
-        E_Float z = EH.z;
-        xt[ind] = x;
-        yt[ind] = y;
-        zt[ind] = z;
-        ind++;
-    }
-
-    // Tag the projected points
-    npy_intp dims[2];
-    dims[1] = 1;
-    dims[0] = (npy_intp)ni*nj*nk;
-    PyArrayObject *tag = (PyArrayObject *)PyArray_SimpleNew(1, dims, NPY_DOUBLE);
-    E_Float *ptag = (E_Float *)PyArray_DATA(tag);
-    for (E_Int i = 0; i < nij*kmax; i++) ptag[i] = 0.0;
-    for (E_Int i = nij*kmax; i < nij*nk; i++) ptag[i] = 1.0;
-
-    PyObject *out = PyList_New(0);
-    PyList_Append(out, tpl);
-    PyList_Append(out, (PyObject *)tag);
-    RELEASESHAREDS(tpl, f);
-    Py_DECREF(tpl);
-    Py_DECREF(tag);
-    
-    return out;
 }
