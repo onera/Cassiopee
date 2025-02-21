495ddc6d81a404546f83807402c3898b35b82f6c
christophe.benoit@onera.fr <christophe.benoit@onera.fr>
2025-02-21 10:50:53 +0100

    OCC: update doc

--- docs/doc/Examples/OCC/splitFaces.py
+++ docs/doc/Examples/OCC/splitFaces.py
@@ -1,5 +1,6 @@
-# - writeCAD (array) -
+# - splitFaces (array) -
 import OCC
 
 hook = OCC.readCAD("cube.step", "fmt_step")
+OCC._splitFaces(hook, 20.)
 OCC.writeCAD(hook, "out.step", "fmt_step")
--- docs/doc/Examples/OCC/writeCADPT.py
+++ docs/doc/Examples/OCC/writeCADPT.py
@@ -1,5 +1,5 @@
-# - writeCAD (array) -
-import OCC
+# - writeCAD (pyTree) -
+import OCC.PyTree as OCC
 
 hook = OCC.readCAD("cube.step", "fmt_step")
 OCC.writeCAD(hook, "out.step", "fmt_step")
