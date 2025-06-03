06070433cfcb8abd671c678a5f0791b543ca5db1
StephaniePERON <stephanie.peron13@gmail.com>
2025-06-03 09:08:48 +0200

    XCore: add unitary tests + XCore.PyTree: comments on functions

--- Cassiopee/XCore/test/adaptMesh_ExitPT.py
+++ Cassiopee/XCore/test/adaptMesh_ExitPT.py
@@ -5,6 +5,7 @@ import Converter.Mpi as Cmpi
 import Converter.Internal as Internal
 import KCore.test as test
 import numpy 
+import KCore.test as test
 
 # 2D QUAD
 a = G.cartHexa((0,0,0),(0.1,0.1,0.1),(11,11,2))
@@ -23,10 +24,4 @@ gcells=numpy.arange(0, ncells)
 gfaces=numpy.arange(1,nfaces+1)
 
 AM = XC.AdaptMesh_Init(a, normal2D, comm=[], gcells=gcells, gfaces=gfaces)
-C._initVars(a, '{centers:indicator}=({centers:CoordinateX}>0.5)')
-f = Internal.getNodeFromName(a, 'indicator')[1]
-REF = f.astype(dtype=Internal.E_NpyInt)
-XC.AdaptMesh_AssignRefData(AM, REF)
-XC.AdaptMesh_Adapt(AM)
-a = XC.AdaptMesh_ExtractMesh(AM, conformize=0)
-C.convertPyTree2File(a,"out.cgns")
+XC.AdaptMesh_Exit(AM)
---
+++ Cassiopee/XCore/test/intersectMeshPT.py
@@ -0,0 +1,46 @@
+import Converter.PyTree as C
+import Converter.Internal as I
+import XCore.PyTree as X
+import Generator.PyTree as G
+import Transform.PyTree as T 
+import Post.PyTree as P
+
+# Load master hexa mesh and transform it to NGon
+m = G.cartNGon((0,0,0),(0.1,0.1,0.1),(11,11,11))
+# Mark the original cells to keep
+C._initVars(m, 'centers:keep', 1.0)
+# Triangulate the external quads
+X._triangulateSkin(m)
+I._adaptNGon42NGon3(m)
+
+# Initialize the IntersectMesh hook
+IM = X.IntersectMesh_Init(m)
+
+# Load the structured meshes to modify
+c = G.cart((0.19,0.17,-0.5),(0.11,0.12,0.1),(5,5,5))
+c = C.newPyTree(['cylinder', c])
+c = G.close(c)
+
+# Remove the K-planes within the master mesh, and project
+cp = X.removeIntersectingKPlanes(IM, c)
+# Mark the original cells to keep
+C._initVars(cp, 'centers:keep', 1.0)
+I._adaptNGon42NGon3(cp)
+
+# Drop the IntersectMesh hook
+X.IntersectMesh_Exit(IM)
+
+# Initialize the adaptation-intersection hook
+IC = X.icapsuleInit2()
+# Set the master mesh
+X.icapsuleSetMaster(IC, m)
+# Set the slave meshes
+X.icapsuleSetSlaves(IC, [cp])
+
+# Intersect
+X.icapsuleIntersect2(IC)
+# Extract the resulting meshes as zones
+Mi = X.icapsuleExtractMaster(IC)
+Si = X.icapsuleExtractSlaves(IC)
+C.convertPyTree2File(Mi, "Mi.cgns")
+C.convertPyTree2File(Si, "Si.cgns")
