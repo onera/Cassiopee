bce23148b143dab9078e613f9fcae806c0d0e33f
StephaniePERON <stephanie.peron13@gmail.com>
2025-07-28 12:11:20 +0200

    Generator: generateAMRMesh for CODA

---
+++ Cassiopee/Generator/test/generateAMRMeshPT_m1.py
@@ -0,0 +1,35 @@
+
+import Converter.PyTree as C
+import Converter.Internal as Internal
+import Converter.Mpi as Cmpi
+import Generator.PyTree as G
+import Geom.PyTree as D 
+import Geom.IBM as D_IBM
+import KCore.test as test
+
+#2D
+a = D.naca(12.)
+dimPb = 2
+D_IBM._setSnear(a, 0.01)
+D_IBM._setIBCType(a,"Musker")
+D_IBM._setDfar(a, 20.)
+
+tb = C.newPyTree(["BODY",a])
+Cmpi.barrier()
+
+t = G.generateAMRMesh(tb, levelMax=10, vmins=[15,10,10,8,5], dim=dimPb, check=False)
+if Cmpi.rank == 0: test.testT(t,1)
+else: test.testT(t,12)
+# 3D
+a = D.sphere((0.,0.,0.),1.)
+dimPb = 3
+D_IBM._setSnear(a, 0.05)
+D_IBM._setIBCType(a,"Musker")
+D_IBM._setDfar(a, 20.)
+
+tb = C.newPyTree(["BODY",a])
+Cmpi.barrier()
+
+t = G.generateAMRMesh(tb, levelMax=5, vmins=[15,10,10,8,5], dim=dimPb, check=False)
+if Cmpi.rank == 0: test.testT(t,2)
+else: test.testT(t,22)
