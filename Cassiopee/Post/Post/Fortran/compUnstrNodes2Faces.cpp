17e7026dd95149ff9f8a257b4f56c8ae2e4f160e
vincentcasseau <hystrath@gmail.com>
2025-08-01 10:17:50 +0200

    WIP conv Post

--- Cassiopee/Post/Post/Fortran/compUnstrNodes2Faces.cpp
+++ Cassiopee/Post/Post/Fortran/compUnstrNodes2Faces.cpp
@@ -17,9 +17,16 @@
     along with Cassiopee.  If not, see <http://www.gnu.org/licenses/>.
 */
 # include "post.h"
+# include "Array/Array.h"
 
-void compUnstrNodes2Faces(
-  K_FLD::FldArrayI& cn, const E_Float* fieldn,
+/* Calcul des valeurs d un champ aux faces des elements a partir des 
+   des valeurs aux noeuds.
+   IN: cn: connectivite elts-noeuds
+   IN: fieldn: champs aux noeuds
+   OUT: fieldf: champs aux centres des faces
+*/
+void K_POST::compUnstrNodes2Faces(
+  K_FLD::FldArrayI& cn, const char* eltType, const E_Float* fieldn,
   E_Float* fieldf
 )
 {
@@ -27,7 +34,14 @@ void compUnstrNodes2Faces(
   std::vector<char*> eltTypes;
   K_ARRAY::extractVars(eltType, eltTypes);
 
-  E_Int elOffset = 0;
+  // Pre-compute element offset
+  vector<E_Int> nepc(nc+1); nepc[0] = 0;;
+  for (E_Int ic = 0; ic < nc; ic++)
+  {
+    K_FLD::FldArrayI& cm = *(cn.getConnect(ic));
+    E_Int nelts = cm.getSize();
+    nepc[ic+1] = nepc[ic] + nelts;
+  }
 
   # pragma omp parallel
   {
@@ -35,6 +49,7 @@ void compUnstrNodes2Faces(
     {
       K_FLD::FldArrayI& cm = *(cn.getConnect(ic));
       E_Int nelts = cm.getSize();
+      E_Int elOffset = nepc[ic];
 
       if (strcmp(eltTypes[ic], "TRI") == 0)
       {
@@ -108,17 +123,99 @@ void compUnstrNodes2Faces(
           fieldf[pos + 3] = K_CONST::ONE_THIRD * (f1 + f3 + f4);  // A1A3A4
         }
       }
-      else if (strcmp(eltTypes[ic], "HEXA") == 0)
+      else if (strcmp(eltTypes[ic], "PYRA") == 0)
       {
-        nfpe = 6;
-        compHexaSurf(cm, fcOffset, xt, yt, zt, surfnx, surfny, surfnz, surface);
-        k6comphexafield(npts, nelts, cn, fieldn, fieldf);
+        E_Int ind1, ind2, ind3, ind4, ind5, pos;
+        E_Float f1, f2, f3, f4, f5;
+
+        # pragma omp for
+        for (E_Int i = 0; i < nelts; i++)
+        {
+          ind1 = cm[i*5] - 1;
+          ind2 = cm[i*5 + 1] - 1;
+          ind3 = cm[i*5 + 2] - 1;
+          ind4 = cm[i*5 + 3] - 1;
+          ind5 = cm[i*5 + 4] - 1;
+
+          f1 = fieldn[ind1];
+          f2 = fieldn[ind2];
+          f3 = fieldn[ind3];
+          f4 = fieldn[ind4];
+          f5 = fieldn[ind5];
+
+          pos = elOffset + i * 5;
+          fieldf[pos] = K_CONST::ONE_FOURTH * (f1 + f2 + f3 + f4);  // A1A2A3A4
+          fieldf[pos + 1] = K_CONST::ONE_THIRD * (f1 + f2 + f5);    // A1A2A5
+          fieldf[pos + 2] = K_CONST::ONE_THIRD * (f2 + f3 + f5);    // A2A3A5
+          fieldf[pos + 3] = K_CONST::ONE_THIRD * (f3 + f4 + f5);    // A3A4A5
+          fieldf[pos + 4] = K_CONST::ONE_THIRD * (f4 + f1 + f5);    // A4A1A5
+        }
       }
+      
       else if (strcmp(eltTypes[ic], "PENTA") == 0)
       {
-        nfpe = 5;
-        compPentaSurf(cm, fcOffset, xt, yt, zt, surfnx, surfny, surfnz, surface);
-        k6comppentafield(npts, nelts, cn, fieldn, fieldf);
+        E_Int ind1, ind2, ind3, ind4, ind5, ind6, pos;
+        E_Float f1, f2, f3, f4, f5, f6;
+
+        # pragma omp for
+        for (E_Int i = 0; i < nelts; i++)
+        {
+          ind1 = cm[i*6] - 1;
+          ind2 = cm[i*6 + 1] - 1;
+          ind3 = cm[i*6 + 2] - 1;
+          ind4 = cm[i*6 + 3] - 1;
+          ind5 = cm[i*6 + 4] - 1;
+          ind6 = cm[i*6 + 5] - 1;
+
+          f1 = fieldn[ind1];
+          f2 = fieldn[ind2];
+          f3 = fieldn[ind3];
+          f4 = fieldn[ind4];
+          f5 = fieldn[ind5];
+          f6 = fieldn[ind6];
+
+          pos = elOffset + i * 5;
+          fieldf[pos] = K_CONST::ONE_THIRD * (f1 + f2 + f3);            // A1A2A3
+          fieldf[pos + 1] = K_CONST::ONE_THIRD * (f4 + f5 + f6);        // A4A5A6
+          fieldf[pos + 2] = K_CONST::ONE_FOURTH * (f1 + f2 + f5 + f4);  // 1254
+          fieldf[pos + 3] = K_CONST::ONE_FOURTH * (f2 + f3 + f6 + f5);  // 2365
+          fieldf[pos + 4] = K_CONST::ONE_FOURTH * (f3 + f1 + f4 + f6);  // 3146
+        }
+      }
+      else if (strcmp(eltTypes[ic], "HEXA") == 0)
+      {
+        E_Int ind1, ind2, ind3, ind4, ind5, ind6, ind7, ind8, pos;
+        E_Float f1, f2, f3, f4, f5, f6, f7, f8;
+
+        # pragma omp for
+        for (E_Int i = 0; i < nelts; i++)
+        {
+          ind1 = cm[i*8] - 1;
+          ind2 = cm[i*8 + 1] - 1;
+          ind3 = cm[i*8 + 2] - 1;
+          ind4 = cm[i*8 + 3] - 1;
+          ind5 = cm[i*8 + 4] - 1;
+          ind6 = cm[i*8 + 5] - 1;
+          ind7 = cm[i*8 + 6] - 1;
+          ind8 = cm[i*8 + 7] - 1;
+
+          f1 = fieldn[ind1];
+          f2 = fieldn[ind2];
+          f3 = fieldn[ind3];
+          f4 = fieldn[ind4];
+          f5 = fieldn[ind5];
+          f6 = fieldn[ind6];
+          f7 = fieldn[ind7];
+          f8 = fieldn[ind8];
+
+          pos = elOffset + i * 6;
+          fieldf[pos] = K_CONST::ONE_FOURTH * (f1 + f2 + f3 + f4);      // A1A2A3A4
+          fieldf[pos + 1] = K_CONST::ONE_FOURTH * (f5 + f6 + f7 + f8);  // A5A6A7A8
+          fieldf[pos + 2] = K_CONST::ONE_FOURTH * (f4 + f1 + f5 + f8);  // 4158
+          fieldf[pos + 3] = K_CONST::ONE_FOURTH * (f2 + f3 + f7 + f6);  // A2A3A7A6
+          fieldf[pos + 4] = K_CONST::ONE_FOURTH * (f1 + f2 + f6 + f5);  // A1A2A6A5
+          fieldf[pos + 5] = K_CONST::ONE_FOURTH * (f3 + f4 + f8 + f7);  // A3A4A8A7
+        }
       }
       else
       {
@@ -126,8 +223,6 @@ void compUnstrNodes2Faces(
         fprintf(stderr, "Unknown type of element.\n");
         exit(0);
       }
-      // Increment element offset
-      elOffset += nelts;
     }
   }
 
