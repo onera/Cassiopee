7b36f5834c59c4db62a5669efff4c10be7d7b57c
antjost <antoinejost.belmonte@gmail.com>
2025-09-04 12:58:29 +0200

    [IBM-AMR] Bug fixes symmetry plane

--- Cassiopee/KCore/Dist.py
+++ Cassiopee/KCore/Dist.py
@@ -9,9 +9,9 @@ from setuptools import Extension
 DEBUG = False
 
 # Toggle to True for compiling all integers in i8
-EDOUBLEINT = True
+EDOUBLEINT = False
 # Toggle to True for compiling global index in i8
-GDOUBLEINT = True
+GDOUBLEINT = False
 
 #==============================================================================
 # Check module import
