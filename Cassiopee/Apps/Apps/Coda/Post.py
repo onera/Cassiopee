c9046897447b2a9aec04290f3596227f3f4582d4
antjost <antoinejost.belmonte@gmail.com>
2025-09-25 17:27:22 +0200

    [AMRIBM] v2

--- Cassiopee/Apps/Apps/Coda/Post.py
+++ Cassiopee/Apps/Apps/Coda/Post.py
@@ -9,13 +9,14 @@ import os, sys, numpy, math
 
 #FSDM imports
 try:
-    from FSDataManager import FSClac, FSError, FSMesh, FSMeshEnums, FSDataName, FSFloatArray, FSStringArray, FSDataSpecArray, FSDatasetInfo, FSQuantityDescArray
+    from FSDataManager import FSClac, FSError, FSMesh, FSMeshEnums, FSDataName, \
+                              FSFloatArray, FSStringArray, FSDataSpecArray, FSDatasetInfo, FSQuantityDescArray
     from CODA import DiscretizationFactory, TimeIntegrationFactory, FSMeshInterpolationVolumeReconstruction
 except:
     raise ImportError("interpolationDonorPoints: FSDataManager and CODA")
 
 
-def interpolationDonorPoints(fsmesh, clac, discParaDict, discSelectionParaDict, marker_IBM, wall_boundary_markers=[], localDir='./', check=False):
+def interpolationDonorPoints(fsmesh, clac, discParaDict, discSelectionParaDict, markerIBM, wallBoundaryMarkers=[], localDir='./', check=False):
     
     try:
         from FSZoltan import FSZoltanInterface  # noqa: 401
@@ -45,35 +46,35 @@ def interpolationDonorPoints(fsmesh, clac, discParaDict, discSelectionParaDict, 
     
     wall        = fsmesh.GetUnstructDataset("WallPointCoordinates").GetValues()
     donor       = fsmesh.GetUnstructDataset("DonorPointCoordinates").GetValues()
-    wall_numpy  = numpy.array(wall.Buffer() , copy=True)
-    donor_numpy = numpy.array(donor.Buffer(), copy=True)
-    nb_vertex   = wall.Size(0)
+    wallNumpy  = numpy.array(wall.Buffer() , copy=True)
+    donorNumpy = numpy.array(donor.Buffer(), copy=True)
     
     #Get marker list
-    fs_boundary_marker_list = fsmesh.GetCellAttributeValuesWithNames("CADGroupID")
-    np_boundary_marker_list = numpy.array(fs_boundary_marker_list.Buffer(), copy=True)
+    fsBoundaryMarkerList = fsmesh.GetCellAttributeValuesWithNames("CADGroupID")
+    npBoundaryMarkerList = numpy.array(fsBoundaryMarkerList.Buffer(), copy=True)
     
-    fs_boundary_markers_celltype = fsmesh.GetCellAttribute("CADGroupID",4)
-    np_boundary_markers_celltype = numpy.array(fs_boundary_markers_celltype.Buffer(), copy=True)
+    fsBoundaryMarkersCelltype = fsmesh.GetCellAttribute("CADGroupID",4)
+    npBoundaryMarkersCelltype = numpy.array(fsBoundaryMarkersCelltype.Buffer(), copy=True)
     
-    indices      = numpy.ravel(numpy.argwhere(np_boundary_markers_celltype==marker_IBM[0]))
+    indices      = numpy.ravel(numpy.argwhere(npBoundaryMarkersCelltype == markerIBM[0]))
     nb_nodes_IBM = len(indices)
-    if wall_boundary_markers != []:
-      wall_numpy_IBM  = numpy.concatenate([wall_numpy[indices], wallBF_numpy[indices_wall]])
-      donor_numpy_IBM = numpy.concatenate([donor_numpy[indices],wallBF_numpy[indices_wall]])
+    if wallBoundaryMarkers != []:
+        ##This makes no sense...i have no indices_wall...taken directory from original toolbox
+        wallNumpyIBM  = numpy.concatenate([wallNumpy[indices],  wallBF_numpy[indices_wall]])
+        donorNumpyIBM = numpy.concatenate([donorNumpy[indices], wallBF_numpy[indices_wall]])
     else:
-      wall_numpy_IBM  = wall_numpy[indices]
-      donor_numpy_IBM = donor_numpy[indices]
+        wallNumpyIBM  = wallNumpy[indices]
+        donorNumpyIBM = donorNumpy[indices]
     
-    fsmesh_dp = FSMesh(clac)
-    fsmesh_dp.BeginInitialization()
-    fsmesh_dp.InitUnstructNodes(nb_nodes_IBM)
+    fsmeshDonorPnt = FSMesh(clac)
+    fsmeshDonorPnt.BeginInitialization()
+    fsmeshDonorPnt.InitUnstructNodes(nb_nodes_IBM)
     
-    fs_coordinates_donor = FSFloatArray(donor_numpy_IBM.shape[0],3)
-    fs_coordinates_wall  = FSFloatArray(wall_numpy_IBM.shape[0] ,3)
+    fsCoordinatesDonor = FSFloatArray(donorNumpyIBM.shape[0], 3)
+    fsCoordinatesWall  = FSFloatArray(wallNumpyIBM.shape[0] , 3)
     
-    numpy.copyto(numpy.array(fs_coordinates_donor.Buffer(), copy=False), donor_numpy_IBM, casting='unsafe')
-    numpy.copyto(numpy.array(fs_coordinates_wall.Buffer() , copy=False), wall_numpy_IBM , casting='unsafe')
+    numpy.copyto(numpy.array(fsCoordinatesDonor.Buffer(), copy=False), donorNumpyIBM, casting='unsafe')
+    numpy.copyto(numpy.array(fsCoordinatesWall.Buffer() , copy=False), wallNumpyIBM , casting='unsafe')
     
     coordNames    = FSStringArray(3)
     coordNames[0] = FSDataName.Coordinate().X()
@@ -85,15 +86,15 @@ def interpolationDonorPoints(fsmesh, clac, discParaDict, discSelectionParaDict, 
     coordSpecs[1].Length()
     coordSpecs[2].Length()
     
-    fsmesh_dp.InitUnstructDataset(FSDataName("DonorPoints"),
+    fsmeshDonorPnt.InitUnstructDataset(FSDataName("DonorPoints"),
                                   FSDatasetInfo(coordNames, coordSpecs, FSMeshEnums.CT_Node),
-                                  fs_coordinates_donor)
+                                  fsCoordinatesDonor)
     
-    fsmesh_dp.InitUnstructDataset(FSDataName("WallPoints"),
+    fsmeshDonorPnt.InitUnstructDataset(FSDataName("WallPoints"),
                                   FSDatasetInfo(coordNames, coordSpecs, FSMeshEnums.CT_Node),
-                                  fs_coordinates_wall)
+                                  fsCoordinatesWall)
     
-    fsmesh_dp.EndInitialization()
+    fsmeshDonorPnt.EndInitialization()
     
     # prepare face-based data for preprocessing
     fsmesh.HasLocalNumbering() or fsmesh.CreateLocalNumbering()
@@ -107,33 +108,33 @@ def interpolationDonorPoints(fsmesh, clac, discParaDict, discSelectionParaDict, 
     # solution transfer and output
     state.ExportToFSMesh(disc.GetMeshInterface(), fsmesh, "State") or FSError.PrintAndExit()
 
-    _checkRepartitionOption(fsmesh_dp,clac)
+    _checkRepartitionOption(fsmeshDonorPnt,clac)
     
     # import mesh of donor points
-    fsmesh_dp.HasLocalNumbering() or fsmesh_dp.CreateLocalNumbering()
+    fsmeshDonorPnt.HasLocalNumbering() or fsmeshDonorPnt.CreateLocalNumbering()
     
-    # prepare for interpolation of quantities from fsmesh to fsmesh_dp
+    # prepare for interpolation of quantities from fsmesh to fsmeshDonorPnt
     srcCoords = FSQuantityDescArray()
     fsmesh.DetermineCoordinates(srcCoords) or FSError.PrintAndExit()
     quantities = FSQuantityDescArray()
     dstCoords  = FSQuantityDescArray()
-    fsmesh_dp.DetermineCoordinates(dstCoords) or FSError.PrintAndExit()
+    fsmeshDonorPnt.DetermineCoordinates(dstCoords) or FSError.PrintAndExit()
     ipvr = FSMeshInterpolationVolumeReconstruction("sourcePoint")
-    ipvr.InterpolateAtNodes(fsmesh, srcCoords, disc, state, fsmesh_dp, dstCoords, quantities, False) or FSError.PrintAndExit()
+    ipvr.InterpolateAtNodes(fsmesh, srcCoords, disc, state, fsmeshDonorPnt, dstCoords, quantities, False) or FSError.PrintAndExit()
     
     if check:
-        fsmesh_dp.ExportMeshTECPLOT(Filename=localDir+'pointMesh_AugState.dat', FileFormat='ASCII',PrefixDatasetName=True) or FSError.PrintAndExit()
-    node_donorpoints = fsmesh_dp.GetUnstructDataset("DonorPoints").GetValues()
-    node_wallpoints  = fsmesh_dp.GetUnstructDataset("WallPoints").GetValues()
-    augState_data    = fsmesh_dp.GetUnstructDataset("CODAAugState").GetValues()
-    augState_names   = fsmesh_dp.GetUnstructDataset("CODAAugState").GetNames()
+        fsmeshDonorPnt.ExportMeshTECPLOT(Filename=localDir+'pointMesh_AugState.dat', FileFormat='ASCII', PrefixDatasetName=True) or FSError.PrintAndExit()
+    nodeDonorPoints = fsmeshDonorPnt.GetUnstructDataset("DonorPoints").GetValues()
+    nodeWallPoints  = fsmeshDonorPnt.GetUnstructDataset("WallPoints").GetValues()
+    augStateData    = fsmeshDonorPnt.GetUnstructDataset("CODAAugState").GetValues()
+    augStateNames   = fsmeshDonorPnt.GetUnstructDataset("CODAAugState").GetNames()
     
-    node_donorpoints = Cmpi.allgather(numpy.array(node_donorpoints.Buffer(), copy=True))
-    node_wallpoints  = Cmpi.allgather(numpy.array(node_wallpoints.Buffer() , copy=True))
-    augState_data    = Cmpi.allgather(numpy.array(augState_data.Buffer()   , copy=True))
+    nodeDonorPoints = Cmpi.allgather(numpy.array(nodeDonorPoints.Buffer(), copy=True))
+    nodeWallPoints  = Cmpi.allgather(numpy.array(nodeWallPoints.Buffer() , copy=True))
+    augStateData    = Cmpi.allgather(numpy.array(augStateData.Buffer()   , copy=True))
 
     
-    return node_donorpoints, node_wallpoints, augState_data
+    return nodeDonorPoints, nodeWallPoints, augStateData
 
 
 def computeSurfValues(fileNameResultIn, tb, CODAInputs, dim=3, fileNameIBMPnts=None, fileNameResultOut=None, localDir='./', check=False, verbose=False):
@@ -142,9 +143,12 @@ def computeSurfValues(fileNameResultIn, tb, CODAInputs, dim=3, fileNameIBMPnts=N
     if isinstance(tb, str): tb = C.convertFile2PyTree(tb)
     else: tb = Internal.copyTree(tb)
 
+    if not isinstance(fileNameResultIn,str):
+        ValueError('fileNameResultIn MUST the name of the file. Importing of this file is done in this file through "fsmesh.ImportMeshHDF5".')
+
     discParaDict            = CODAInputs[0]
     discSelectionParaDict   = CODAInputs[1]
-    IBM_markers             = CODAInputs[2]
+    IBMMarkers              = CODAInputs[2]
     dictReferenceQuantities = CODAInputs[3]
     
     clac   = FSClac()
@@ -152,19 +156,20 @@ def computeSurfValues(fileNameResultIn, tb, CODAInputs, dim=3, fileNameIBMPnts=N
     fsmesh.ImportMeshHDF5(Filename=localDir+fileNameResultIn) or FSError.PrintAndExit()
     
     # Reconstruction of the solution at the donor points (in parallel).
-    donorPoints_numpy, wallPoints_numpy, augState_numpy = interpolationDonorPoints(fsmesh, clac, discParaDict, discSelectionParaDict, IBM_markers, localDir=localDir, check=check)
+    donorPointsNumpy, wallPointsNumpy, augStateNumpy = interpolationDonorPoints(fsmesh, clac, discParaDict, discSelectionParaDict, 
+                                                                                IBMMarkers, localDir=localDir, check=check)
     
     # Serial part of the post-processing.
     zw = None
     if Cmpi.master:
-        pytree = P_AMR.createPyTreeForIBMWallFieldsExtraction(numpy.concatenate(donorPoints_numpy),
-                                                        numpy.concatenate(wallPoints_numpy),
-                                                        numpy.concatenate(augState_numpy),
-                                                        discSelectionParaDict)
-        if fileNameIBMPnts is not None: C.convertPyTree2File(pytree,localDir+fileNameIBMPnts)
-        zw = P_AMR.extractIBMWallFields(pytree,tb,discSelectionParaDict)
+        pytree = P_AMR.createPyTreeForIBMWallFieldsExtraction(numpy.concatenate(donorPointsNumpy),
+                                                              numpy.concatenate(wallPointsNumpy),
+                                                              numpy.concatenate(augStateNumpy),
+                                                              discSelectionParaDict)
+        if fileNameIBMPnts is not None: C.convertPyTree2File(pytree, localDir+fileNameIBMPnts)
+        zw = P_AMR.extractIBMWallFields(pytree, tb, discSelectionParaDict)
     
-    zw       = Cmpi.bcast(zw,root=0)    
+    zw       = Cmpi.bcast(zw, root=0)    
     zw,coefs = P_AMR.computeBoundaryQuantities(zw, dictReferenceQuantities, dim=dim, verbose=verbose)
     
     if Cmpi.master:
@@ -177,7 +182,7 @@ def computeSurfValues(fileNameResultIn, tb, CODAInputs, dim=3, fileNameIBMPnts=N
         print("CLpres=  %g"%coefs[5])
         
     if fileNameResultOut is not None:
-        if Cmpi.master: C.convertPyTree2File(zw,localDir+"out_wallNEW.plt")
+        if Cmpi.master: C.convertPyTree2File(zw, localDir+fileNameResultOut)
         return None
     else:
         return zw, coefs
@@ -185,38 +190,36 @@ def computeSurfValues(fileNameResultIn, tb, CODAInputs, dim=3, fileNameIBMPnts=N
 ##========================================================================
 ##========================================================================
 
-def test_MPI_Part1(clac):
+def testMPIPart1(clac):
     #################### TEST MPI ########################### (temporary)
     nProcs = clac.GetNProcs()
     if clac.GetProcID()<10:
-      indices_testMPI = FSFloatArray(2)
-      indices_testMPI[0] = clac.GetProcID()
-      indices_testMPI[1] = clac.GetProcID()+10
+      indicesTestMPI = FSFloatArray(2)
+      indicesTestMPI[0] = clac.GetProcID()
+      indicesTestMPI[1] = clac.GetProcID()+10
     else:
-      indices_testMPI = FSFloatArray(2)
-      indices_testMPI.Fill(0.)
+      indicesTestMPI = FSFloatArray(2)
+      indicesTestMPI.Fill(0.)
     #########################################################
-    indices_testMPI_numpy     = numpy.array(indices_testMPI.Buffer(), copy=True)
-    indices_testMPI_gathered  = Cmpi.allgather(indices_testMPI_numpy)
-    return indices_testMPI_gathered
+    indicesTestMPINumpy     = numpy.array(indicesTestMPI.Buffer(), copy=True)
+    indicesTestMPIGathered  = Cmpi.allgather(indicesTestMPINumpy)
+    return indicesTestMPIGathered
 
 
-def test_MPI_Part2(indices_testMPI_numpy, nProcs):
+def testMPIPart2(indicesTestMPINumpy, nProcs):
   ########## TEST MPI ###########   (temporary)
-  #print("output concatenate",numpy.concatenate(indices_testMPI_numpy))
-  #print(nProcs)
   if nProcs>9:
-    test_array_indices = [i for i in range(0,10)]
+    testArrayIndices = [i for i in range(0,10)]
   else:
-    test_array_indices = [i for i in range(0,nProcs)]
-  full_test_array_indices = numpy.zeros(nProcs*2)
+    testArrayIndices = [i for i in range(0,nProcs)]
+  fullTestArrayIndices = numpy.zeros(nProcs*2)
   counter = 0
-  for i in test_array_indices:
-    full_test_array_indices[counter] = i
-    full_test_array_indices[counter+1] = i+10
+  for i in testArrayIndices:
+    fullTestArrayIndices[counter] = i
+    fullTestArrayIndices[counter+1] = i+10
     counter = counter+2
-  if numpy.equal(numpy.concatenate(indices_testMPI_numpy),full_test_array_indices).all()==False:
-    print("Error: full test array indices", full_test_array_indices)
+  if numpy.equal(numpy.concatenate(indicesTestMPINumpy),fullTestArrayIndices).all()==False:
+    print("Error: full test array indices", fullTestArrayIndices)
     sys.exit("Problem with MPI. Gather not in the correct order")
   ##############################
 
