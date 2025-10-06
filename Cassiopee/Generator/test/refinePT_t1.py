9361c8fdbb0be9b7cf9d131a7eac650fd68e7d27
StephaniePERON <stephanie.peron13@gmail.com>
2025-10-06 12:01:01 +0200

    Generator:refine: refinePT_t1: additional tests

--- Cassiopee/Generator/test/refinePT_t1.py
+++ Cassiopee/Generator/test/refinePT_t1.py
@@ -6,26 +6,53 @@ import KCore.test as test
 # cas 2D multibloc avec CL et raccords
 a = G.cart( (0,0,0), (0.1,0.1,0.1), (11,11,1))
 b = G.cart( (0,1,0), (0.1,0.1,0.1), (11,11,1))
-a = C.addBC2Zone(a,'match1','BCMatch','jmax',b,'jmin')
-a = C.addBC2Zone(a,"wall",'BCWall','imin')
-a = C.addBC2Zone(a,"overlap",'BCOverlap','imax')
-b = C.addBC2Zone(b,'match2','BCMatch','jmin',a,'jmax')
-b = C.addBC2Zone(b,"wall",'BCWall','imin')
-b = C.addBC2Zone(b,"overlap",'BCOverlap','imax')
+C._addBC2Zone(a,'match1','BCMatch','jmax',b,'jmin')
+C._addBC2Zone(a,"wall",'BCWall','imin')
+C._addBC2Zone(a,"overlap",'BCOverlap','imax')
+C._addBC2Zone(b,'match2','BCMatch','jmin',a,'jmax')
+C._addBC2Zone(b,"wall",'BCWall','imin')
+C._addBC2Zone(b,"overlap",'BCOverlap','imax')
 t = C.newPyTree(['Base',2]); t[2][1][2] += [a,b]
-t = C.initVars(t,'F',1.); t = C.initVars(t,'centers:G',2.)
-# facteur de raffinement non entier
+C._initVars(t,'F',1.); C._initVars(t,'centers:G',2.)
+# facteur de raffinement non entier > 1
 for noz in range(2): t[2][1][2][noz] = G.refine(t[2][1][2][noz],1.5,dir=1)
 test.testT(t,1)
+
 # facteur de raffinement entier 1 direction
 t = C.newPyTree(['Base']); t[2][1][2]+=[a,b]
-t = C.initVars(t,'F',1.); t = C.initVars(t,'centers:G',2.)
+C._initVars(t,'F',1.); C._initVars(t,'centers:G',2.)
 for noz in range(2): t[2][1][2][noz] = G.refine(t[2][1][2][noz],2.,dir=1)
 test.testT(t,2)
 #
 # facteur de raffinement entier 3 directions
 #
 t = C.newPyTree(['Base',2]); t[2][1][2] += [a,b]
-t = C.initVars(t,'F',1.); t = C.initVars(t,'centers:G',2.)
+C._initVars(t,'F',1.); C._initVars(t,'centers:G',2.)
 for noz in range(2): t[2][1][2][noz] = G.refine(t[2][1][2][noz],2,dir=0)
 test.testT(t,3)
+
+# facteur de raffinement (1/factor entier) 1 direction
+t = C.newPyTree(['Base']); t[2][1][2]+=[a,b]
+C._initVars(t,'F',1.); C._initVars(t,'centers:G',2.)
+for noz in range(2): t[2][1][2][noz] = G.refine(t[2][1][2][noz],0.5,dir=1)
+test.testT(t,4)
+
+# facteur de raffinement (1/factor non entier) 1 direction
+t = C.newPyTree(['Base']); t[2][1][2]+=[a,b]
+C._initVars(t,'F',1.); C._initVars(t,'centers:G',2.)
+for noz in range(2): t[2][1][2][noz] = G.refine(t[2][1][2][noz],0.3,dir=1)
+test.testT(t,5)
+
+# facteur de raffinement (1/factor entier) 3 directions
+t = C.newPyTree(['Base']); t[2][1][2]+=[a,b]
+C._initVars(t,'F',1.); C._initVars(t,'centers:G',2.)
+for noz in range(2): t[2][1][2][noz] = G.refine(t[2][1][2][noz],0.5,dir=0)
+test.testT(t,6)
+C.convertPyTree2File(t,"out.cgns")
+
+# facteur de raffinement (1/factor non entier) 3 directions
+t = C.newPyTree(['Base']); t[2][1][2]+=[a,b]
+C._initVars(t,'F',1.); C._initVars(t,'centers:G',2.)
+for noz in range(2): t[2][1][2][noz] = G.refine(t[2][1][2][noz],0.3,dir=0)
+test.testT(t,7)
+
