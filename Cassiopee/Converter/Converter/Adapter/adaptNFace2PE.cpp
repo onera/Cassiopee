onera/Cassiopee

commit 06eba8a02dc8b6a96b6a0900524ada7e28cd8b34

Converter: Internal: add dicts mapping eltNo to eltInfo and eltName to eltInfo (#500)
* Converter: Internal: add dicts mapping eltNo to eltInfo and eltName to eltInfo

* Apply autopep8 formatting

* minor

* Apply autopep8 formatting

* minor

* Apply autopep8 formatting

* Converter: add all CGNS element names to ELTNO2ELTINFO

---------

Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

Cassiopee/Converter/Converter/Adapter/adaptNFace2PE.cpp
@@ -30,8 +30,9 @@ PyObject* K_CONVERTER::adaptNFace2PE(PyObject* self, PyObject* args)
 {
   PyObject* arrayNF; PyObject* arrayNG;  
   E_Int nfaces; E_Int nelts;
-  E_Int methodPE(0);
+  E_Int methodPE = 0;
   PyObject* arrayX; PyObject* arrayY; PyObject* arrayZ;
+
   if (!PYPARSETUPLE_(args, OOOO_ O_ III_, &arrayNF,  &arrayNG, 
                      &arrayX, &arrayY, &arrayZ, 
                      &nelts, &nfaces, &methodPE)) return NULL;
@@ -94,11 +95,17 @@ PyObject* K_CONVERTER::adaptNFace2PE(PyObject* self, PyObject* args)
   PyObject* tpl = K_NUMPY::buildNumpyArray(nfaces, 2, 1, 1);
   E_Int* cFE = K_NUMPY::getNumpyPtrI(tpl);
 
-  bool is_3D = (cNGon->begin()[0] > 2);
-  if (!is_3D) methodPE = 0;
-
-  if (methodPE == 0) // GEOMETRIC APPROACH => assume all cells are centroid-star-shaped.
+  if (cNGon->begin()[0] > 2 && methodPE != 0)
   {
+    printf("adaptNFace2PE: NGon connectivity is not 3D, defaulting to method 0 "
+           "(geometric) in which all elements are assumed to be "
+           "centroid-star-shaped.");
+    methodPE = 0;
+  }
+
+  if (methodPE == 0) 
+  {
+    // GEOMETRIC APPROACH => assume all cells are centroid-star-shaped.
     E_Int* facesp1 = cFE;
     E_Int* facesp2 = cFE + nfaces;
 
@@ -338,7 +345,7 @@ PyObject* K_CONVERTER::adaptNFace2PE(PyObject* self, PyObject* args)
       RELEASESHAREDN(arrayX, coordX);
       RELEASESHAREDN(arrayY, coordY);
       PyErr_SetString(PyExc_TypeError, 
-                      "adaptNFace2PE: method 2: failed to reorient external faces.");
+                      "adaptNFace2PE: method 1: failed to reorient external faces.");
       return NULL;
     }
 

Cassiopee/CPlot/apps/tkTree.py
@@ -905,7 +905,7 @@ class Node:
             val = pid[1]
             connectType = val[0]; boundary = val[1]
             v = 'Connectivity of type '
-            name, nnodes = Internal.eltNo2EltName(connectType)
+            name, _ = Internal.eltNo2EltName(connectType)
             v += name
 
             if boundary > 0: v += ' (boundary)'

Cassiopee/Compressor/Compressor/PyTree.py
@@ -539,7 +539,7 @@ def _compressElements(t):
                 n = Internal.getNodeFromName1(e, 'ElementConnectivity')
                 _packNode(n, 0, 4)
             else:
-                (stype, net) = Internal.eltNo2EltName(eltno)
+                _, net = Internal.eltNo2EltName(eltno)
                 n = Internal.getNodeFromName1(e, 'ElementConnectivity')
                 _packNode(n, net, 3)
     return None

Cassiopee/Converter/Converter/Filter2.py
@@ -592,7 +592,7 @@ def createZoneStdElementsFilter(elmt, zone_path, hdf_filter):
     distrib_elmt = I.getVal(getDistribution(elmt, 'Element'))
     dn_elmt      = distrib_elmt[1] - distrib_elmt[0]
 
-    name, elmt_npe = I.eltNo2EltName(elmt[1][0]) # nbre de vertex de l'element
+    _, elmt_npe = I.eltNo2EltName(elmt[1][0]) # nbre de vertex de l'element
 
     DSMMRYElmt = [[0                       ], [1], [dn_elmt*elmt_npe], [1]]
     DSFILEElmt = [[distrib_elmt[0]*elmt_npe], [1], [dn_elmt*elmt_npe], [1]]

Cassiopee/Converter/Converter/IO/GenIO_hdffsdm.cpp
@@ -1115,10 +1115,22 @@ E_Int K_IO::GenIO::hdffsdmwrite(char* file, PyObject* tree)
   hid_t ds = H5Gcreate(uc, "Datasets", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
   // Write Coordinates
+  const char* defaultCoordsName = "Coordinates";
   PyObject* gc = K_PYTREE::getNodeFromType1(zone, "GridCoordinates_t");
   char* coordsName = K_PYTREE::getNodeName(gc);
-  if (strcmp(coordsName, "GridCoordinates") == 0) coordsName = "Coordinates";
-  hid_t coord = H5Gcreate(ds, coordsName, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  char* tmpCoordsName;
+  if (strcmp(coordsName, "GridCoordinates") == 0)
+  {
+    tmpCoordsName = new char[strlen(defaultCoordsName) + 1];
+    strcpy(tmpCoordsName, defaultCoordsName);
+  }
+  else
+  {
+    tmpCoordsName = new char[strlen(coordsName) + 1];
+    strcpy(tmpCoordsName, coordsName);
+  }
+  hid_t coord = H5Gcreate(ds, tmpCoordsName, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  delete [] tmpCoordsName;
   dims[0] = 1;
   did = H5Screate_simple(1, dims, NULL);
 #ifdef E_DOUBLEINT

Cassiopee/Converter/Converter/Internal.py
@@ -5,17 +5,17 @@ import fnmatch # unix wildcards
 import KCore.kcore as KCore
 from . import converter
 
-# INT size for numpys connectivities
+# INT size for numpy connectivities
 from KCore.Dist import EDOUBLEINT
 if EDOUBLEINT: E_NpyInt = numpy.int64
 else: E_NpyInt = numpy.int32
 
-# Containeurs
+# Containers
 __GridCoordinates__ = 'GridCoordinates'
 __FlowSolutionNodes__ = 'FlowSolution'
 __FlowSolutionCenters__ = 'FlowSolution#Centers'
 
-# Pour les BCs non structurees (nommage de transition courant/derniere norme)
+# Boundary condition keywords for unstructured grids (nommage de transition courant/derniere norme)
 __ELEMENTRANGE__ = 'ElementRange' # ElementRange/PointRange
 __ELEMENTLIST__ = 'PointList' # ElementList/PointList
 __FACELIST__ = 'PointList' # FaceList/PointList
@@ -28,64 +28,127 @@ SEP1 = '\\'
 # Separateur intra-BCs (pour les exports getBCFaces et addBCFaces)
 SEP2 = '@'
 
-# other var names to CGNS var names
-name2CGNS = { \
-    'x'              :'CoordinateX'                   , \
-    'y'              :'CoordinateY'                   , \
-    'z'              :'CoordinateZ'                   , \
-    'x3d'            :'CoordinateX'                   , \
-    'y3d'            :'CoordinateY'                   , \
-    'z3d'            :'CoordinateZ'                   , \
-    'X'              :'CoordinateX'                   , \
-    'Y'              :'CoordinateY'                   , \
-    'Z'              :'CoordinateZ'                   , \
-    'ro'             :'Density'                       , \
-    'rou'            :'MomentumX'                     , \
-    'rov'            :'MomentumY'                     , \
-    'row'            :'MomentumZ'                     , \
-    'rovx'           :'MomentumX'                     , \
-    'rovy'           :'MomentumY'                     , \
-    'rovz'           :'MomentumZ'                     , \
-    'roe'            :'EnergyStagnationDensity'       , \
-    'roE'            :'EnergyStagnationDensity'       , \
-    'rok'            :'TurbulentEnergyKineticDensity' , \
-    'ronutilde'      :'TurbulentSANuTildeDensity'    , \
-    'roeps'          :'TurbulentDissipationDensity'   , \
-    'roomega'        :'TurbulentDissipationRateDensity', \
-    'mach'           :'Mach'                          , \
-    'psta'           :'Pressure'                      , \
-    'tsta'           :'Temperature'                   , \
-    'viscrapp'       :'Viscosity_EddyMolecularRatio'  , \
-    'walldistance'   :'TurbulentDistance'             , \
-    'wallglobalindex':'TurbulentDistanceIndex'        , \
+# Map variable names to their CGNS counterparts
+VARNAME2CGNS = {
+    'x'              : 'CoordinateX',
+    'y'              : 'CoordinateY',
+    'z'              : 'CoordinateZ',
+    'x3d'            : 'CoordinateX',
+    'y3d'            : 'CoordinateY',
+    'z3d'            : 'CoordinateZ',
+    'X'              : 'CoordinateX',
+    'Y'              : 'CoordinateY',
+    'Z'              : 'CoordinateZ',
+    'ro'             : 'Density',
+    'rou'            : 'MomentumX',
+    'rov'            : 'MomentumY',
+    'row'            : 'MomentumZ',
+    'rovx'           : 'MomentumX',
+    'rovy'           : 'MomentumY',
+    'rovz'           : 'MomentumZ',
+    'roe'            : 'EnergyStagnationDensity',
+    'roE'            : 'EnergyStagnationDensity',
+    'rok'            : 'TurbulentEnergyKineticDensity',
+    'ronutilde'      : 'TurbulentSANuTildeDensity',
+    'roeps'          : 'TurbulentDissipationDensity',
+    'roomega'        : 'TurbulentDissipationRateDensity',
+    'mach'           : 'Mach',
+    'psta'           : 'Pressure',
+    'tsta'           : 'Temperature',
+    'viscrapp'       : 'Viscosity_EddyMolecularRatio',
+    'walldistance'   : 'TurbulentDistance',
+    'wallglobalindex': 'TurbulentDistanceIndex',
 }
 
-# Known BCs
-KNOWNBCS = ['BCWall', 'BCWallInviscid','BCWallViscous', 'BCWallViscousIsothermal',
-            'BCFarfield', 'BCExtrapolate',
-            'BCInflow', 'BCInflowSubsonic', 'BCInflowSupersonic',
-            'BCOutflow', 'BCOutflowSubsonic', 'BCOutflowSupersonic',
-            'BCMatch', 'BCNearMatch', 'BCOverlap', 'BCSymmetryPlane',
-            'BCDegenerateLine', 'BCDegeneratePoint', 'BCStage',
-            'UserDefined']
+# Known CGNS BC types
+KNOWNBCS = [
+    'BCWall', 'BCWallInviscid','BCWallViscous', 'BCWallViscousIsothermal',
+    'BCFarfield', 'BCExtrapolate',
+    'BCInflow', 'BCInflowSubsonic', 'BCInflowSupersonic',
+    'BCOutflow', 'BCOutflowSubsonic', 'BCOutflowSupersonic',
+    'BCMatch', 'BCNearMatch', 'BCOverlap', 'BCSymmetryPlane',
+    'BCDegenerateLine', 'BCDegeneratePoint', 'BCStage',
+    'UserDefined'
+]
 
-__DEG2RAD__ = 0.017453292519943295 #math.pi/180.
-__RAD2DEG__ = 57.29577951308232    #180./math.pi
+# Dictionary mapping an Element number to a tuple storing
+# (Element name, number of nodes, dimensionality). The number of nodes and the
+# dimensionality are set to -1 and 3, respectively, when they cannot be inferred
+# from the Element number alone (e.g., NGON, MIXED).
+ELTNO2ELTINFO = {
+    2: ('NODE', 1, 0),
+    3: ('BAR', 2, 1),
+    4: ('BAR_3', 3, 1),
+    24: ('BAR_4', 4, 1),
+    40: ('BAR_5', 5, 1),
+    5: ('TRI', 3, 2),
+    6: ('TRI_6', 6, 2),
+    25: ('TRI_9', 9, 2),
+    26: ('TRI_10', 10, 2),
+    41: ('TRI_12', 12, 2),
+    42: ('TRI_15', 15, 2),
+    7: ('QUAD', 4, 2),
+    8: ('QUAD_8', 8, 2),
+    9: ('QUAD_9', 9, 2),
+    27: ('QUAD_12', 12, 2),
+    28: ('QUAD_16', 16, 2),
+    43: ('QUAD_P4_16', 16, 2),
+    44: ('QUAD_25', 25, 2),
+    10: ('TETRA', 4, 3),
+    11: ('TETRA_10', 10, 3),
+    29: ('TETRA_16', 16, 3),
+    30: ('TETRA_20', 20, 3),
+    45: ('TETRA_22', 22, 3),
+    46: ('TETRA_34', 34, 3),
+    47: ('TETRA_35', 35, 3),
+    12: ('PYRA', 5, 3),
+    21: ('PYRA_13', 13, 3),
+    13: ('PYRA_14', 14, 3),
+    31: ('PYRA_21', 21, 3),
+    32: ('PYRA_29', 29, 3),
+    33: ('PYRA_30', 30, 3),
+    48: ('PYRA_P4_29', 29, 3),
+    49: ('PYRA_50', 50, 3),
+    50: ('PYRA_55', 55, 3),
+    14: ('PENTA', 6, 3),
+    15: ('PENTA_15', 15, 3),
+    16: ('PENTA_18', 18, 3),
+    34: ('PENTA_24', 24, 3),
+    51: ('PENTA_33', 33, 3),
+    35: ('PENTA_38', 38, 3),
+    36: ('PENTA_40', 40, 3),
+    52: ('PENTA_66', 66, 3),
+    53: ('PENTA_75', 75, 3),
+    17: ('HEXA', 8, 3),
+    18: ('HEXA_20', 20, 3),
+    19: ('HEXA_27', 27, 3),
+    37: ('HEXA_32', 32, 3),
+    54: ('HEXA_44', 44, 3),
+    38: ('HEXA_56', 56, 3),
+    39: ('HEXA_64', 64, 3),
+    55: ('HEXA_98', 98, 3),
+    56: ('HEXA_125', 125, 3),
+    20: ('MIXED', -1, 3),
+    22: ('NGON', -1, 3),
+    23: ('NFACE', -1, 3)
+}
 
-# Elements dimension by elt no
-DIMELTS = [0,
-           0,0,1,1,2,2,2,2,2,3,
-           3,3,3,3,3,3,3,3,3,3,
-           3,3,3,1,2,2,2,2,3,3,
-           3,3,3,3,3,3,3,3,3,1,
-           2,2,2,2,3,3,3,3,3,3,
-           3,3,3,3,3,3]
+# Dictionary mapping an Element name to a tuple storing
+# (Element number, number of nodes). The number of nodes is set to -1 when it
+# cannot be inferred from the Element name alone (e.g., NGON, MIXED).
+ELTNAME2ELTINFO = {
+    eltInfo[0]: (eltNo, eltInfo[1]) for eltNo, eltInfo in ELTNO2ELTINFO.items()
+}
+
+__DEG2RAD__ = 0.017453292519943295 # math.pi/180.
+__RAD2DEG__ = 57.29577951308232    # 180./math.pi
+
 
 #==============================================================================
-# -- is? --
+# -- Test node equality --
 #==============================================================================
 
-# -- is node a top tree or something else?
+# -- is node a top tree?
 def isTopTree(node):
   """Return True if node corresponds to a top tree node (CGSNTree_t)."""
   if len(node) != 4: return False
@@ -97,7 +160,8 @@ def isTopTree(node):
 # Retourne -1 si node est un noeud standard de l'arbre
 # Retourne 0 si node est une liste de noeuds standards (meme vide)
 def isStdNode(node):
-  """Return 0 if node is a list of standard pyTree nodes, -1 if node is a standard pyTree node, -2 otherwise."""
+  """Return 0 if node is a list of standard pyTree nodes,
+  -1 if node is a standard pyTree node, -2 otherwise."""
   if not isinstance(node, list): return -2
   if len(node) == 0: return 0
   node0 = node[0]
@@ -671,7 +735,7 @@ def newElements(name='Elements', etype='UserDefined',
                 erange=None, eboundary=0, parent=None):
   """Create a new Elements node."""
   if isinstance(etype, int): etp = etype
-  else: etp, nnodes = eltName2EltNo(etype)
+  else: etp, _ = eltName2EltNo(etype)
   if parent is None:
     node = createNode(name, 'Elements_t', value=[etp,eboundary])
   else:
@@ -2460,7 +2524,7 @@ def copyRef(node):
 
 # -- Copie un arbre ou un sous-arbre en copiant aussi les numpy.array
 def copyTree(node, order='F'):
-  """Fully copy a tree."""
+  """Full copy of a tree."""
   ret = isStdNode(node)
   if ret == -1:
     d = copyTree__(node, order=order)
@@ -2483,7 +2547,7 @@ def copyTree__(node, parent=None, order='F'):
 
 # -- Copie un noeud (pas de recursivite)
 def copyNode(node):
-  """Copy only this node (no recursion). Share children with node. """
+  """Copy only this node (no recursion). Share children with node."""
   ret = isStdNode(node)
   if ret == -1:
     if node[1] is not None and isinstance(node[1], numpy.ndarray):
@@ -2536,13 +2600,15 @@ def merge(A, pathList=None):
   # check
   if pathList:
     for path in pathList:
-      if not isinstance(path,str): raise AttributeError("merge: pathList should be a list of strings.")
+      if not isinstance(path,str):
+        raise AttributeError("merge: pathList should be a list of strings.")
   tp = copyRef(A[0])
   if pathList is None: pathList = [None for tree in A[1:]]
   for path, tree in zip(pathList, A[1:]):
     if path: _append(tp, tree, path)
     else:
-      if tp[0] != tree[0]: raise AttributeError("merge: path is None, but root name are differents.")
+      if tp[0] != tree[0]:
+        raise AttributeError("merge: path is None, but root name are differents.")
       for child in tree[2]: _append(tp, child, tree[0])
   return tp
 
@@ -2692,11 +2758,20 @@ def pyTree2Node(t, type):
   else: node = t
   return node
 
+# -- EltName2EltNo
+# Convertit un nom CGNS d'elt en no CGNS d'elt et son nombre de noeuds associes
+def eltName2EltNo(name):
+  if name.endswith('*'): name = name[:-1]
+  eltno, nnodes = ELTNAME2ELTINFO.get(name, ('UNKNOWN', -1))
+  if name == 'MIXED':
+    print('Warning: eltName2EltNo: MIXED elements not supported.')
+  return eltno, nnodes
+
 # -- EltNames2EltNos
 # Convertit une liste de noms CGNS d'elt en nos CGNS d'elt et leurs nombres
 # de noeuds associes. Retourne deux listes
 def eltNames2EltNos(names):
-  if (isinstance(names, (list, tuple, numpy.ndarray)) and len(names) and
+  if (isinstance(names, (list, tuple)) and len(names) and
           isinstance(names[0], str)):
     names = ','.join(name for name in names)
   if len(names.split(',')) > 1:
@@ -2704,63 +2779,14 @@ def eltNames2EltNos(names):
     return list(zip(*eltnos))
   else: return [[i] for i in eltName2EltNo(names)]
 
-# -- EltName2EltNo
-# Convertit un nom CGNS d'elt en no CGNS d'elt et son nombre de noeuds associes
-def eltName2EltNo(name):
-  eltno = 0; nnodes = 0
-  if name == 'NODE':
-    eltno = 2; nnodes = 1
-  elif name[0:3] == 'BAR':
-    if len(name) == 3: nnodes = 2
-    else: nnodes = int(name[4:])
-    if nnodes == 2: eltno = 3
-    elif nnodes == 3: eltno = 4
-    elif nnodes == 4: eltno = 24
-  elif name[0:3] == 'TRI':
-    if len(name) == 3: nnodes = 3
-    else: nnodes = int(name[4:])
-    if nnodes == 3: eltno = 5
-    elif nnodes == 6: eltno = 6
-    elif nnodes == 9: eltno = 25   # D'apres l'enumeration CGNS
-    elif nnodes == 10: eltno = 26  #
-    # Pour l'ordre 4, rien dans l'enumeration CGNS...
-  elif name[0:4] == 'QUAD':
-    if len(name) == 4: nnodes = 4
-    else: nnodes = int(name[5:])
-    if nnodes == 4: eltno = 7
-    elif nnodes == 8:  eltno = 8
-    elif nnodes == 9:  eltno = 9
-    elif nnodes == 12: eltno = 27 # D'apres l'enumeration CGNS
-    elif nnodes == 16: eltno = 28 # Attention, ici, je prends par defaut QUAD_16 mais cela pourrait etre QUAD_P4_16... Que faire ?
-    # Pour l'ordre 4, rien dans l'enumeration CGNS
-  elif name[0:5] == 'TETRA':
-    if len(name) == 5: nnodes = 4
-    else: nnodes = int(name[6:])
-    if nnodes == 4: eltno = 10
-    elif nnodes == 10: eltno = 11
-  elif name[0:4] == 'PYRA':
-    if len(name) == 4: nnodes = 5
-    else: nnodes = int(name[5:])
-    if nnodes == 5: eltno = 12
-    elif nnodes == 14: eltno = 13
-  elif name[0:5] == 'PENTA':
-    if len(name) == 5: nnodes = 6
-    else: nnodes = int(name[6:])
-    if nnodes == 6: eltno = 14
-    elif nnodes == 15: eltno = 15
-    elif nnodes == 18: eltno = 16
-  elif name[0:4] == 'HEXA':
-    if len(name) == 4: nnodes = 8
-    else: nnodes = int(name[5:])
-    if nnodes == 8: eltno = 17
-    elif nnodes == 20: eltno = 18
-    elif nnodes == 27: eltno = 19
-  elif name == 'MIXED':
-    print('Warning: eltName2EltNo: MIXED elements not supported.')
-    eltno = 20; nnodes = 1
-  elif name == 'NGON' or name == 'NGON_n': eltno = 22; nnodes = 1
-  elif name == 'NFACE' or name == 'NFACE_n': eltno = 23; nnodes = 1
-  return eltno, nnodes
+# -- EltNo2EltName
+# Convertit un numero CGNS d'elt en nom CGNS d'elt et son nombre de noeuds
+# associes
+def eltNo2EltName(eltno):
+  name, nnodes, _ = ELTNO2ELTINFO.get(eltno, ('UNKNOWN', -1, 3))
+  if eltno == 20:
+    print('Warning: eltNo2EltName: MIXED elements not supported.')
+  return name, nnodes
 
 # -- EltNos2EltNames
 # Convertit une liste de numeros CGNS d'elt en noms CGNS d'elt et leurs nombres
@@ -2771,49 +2797,22 @@ def eltNos2EltNames(eltnos):
     return list(zip(*eltnames))
   else: return [[i] for i in eltNo2EltName(eltnos)]
 
-# -- EltNo2EltName
-# Convertit un numero CGNS d'elt en nom CGNS d'elt et son nombre de noeuds
-# associes
-def eltNo2EltName(eltno):
-  name = 'UNKNOWN'; nnodes = 0
-  if eltno == 2: name = 'NODE'; nnodes = 1
-  elif eltno == 3: name = 'BAR'; nnodes = 2
-  elif eltno == 4: name = 'BAR_3'; nnodes = 3
-  elif eltno == 24: name = 'BAR_4'; nnodes = 4
-  elif eltno == 5: name = 'TRI'; nnodes = 3
-  elif eltno == 6: name = 'TRI_6'; nnodes = 6
-  elif eltno == 25: name = 'TRI_9'; nnodes = 9
-  elif eltno == 26: name = 'TRI_10'; nnodes = 10
-  elif eltno == 7: name = 'QUAD'; nnodes = 4
-  elif eltno == 8: name = 'QUAD_8'; nnodes = 8
-  elif eltno == 9: name = 'QUAD_9'; nnodes = 9
-  elif eltno == 27: name = 'QUAD_12'; nnodes = 12
-  elif eltno == 28: name = 'QUAD_16'; nnodes = 16
-  elif eltno == 10: name = 'TETRA'; nnodes = 4
-  elif eltno == 11: name = 'TETRA_10'; nnodes = 10
-  elif eltno == 12: name = 'PYRA'; nnodes = 5
-  elif eltno == 13: name = 'PYRA_14'; nnodes = 14
-  elif eltno == 14: name = 'PENTA'; nnodes = 6
-  elif eltno == 15: name = 'PENTA_15'; nnodes = 15
-  elif eltno == 16: name = 'PENTA_18'; nnodes = 18
-  elif eltno == 17: name = 'HEXA'; nnodes = 8
-  elif eltno == 18: name = 'HEXA_20'; nnodes = 20
-  elif eltno == 19: name = 'HEXA_27'; nnodes = 27
-  elif eltno == 20:
-    print('Warning: eltNo2EltName: MIXED elements not supported.')
-    name = 'MIXED'; nnodes = 1
-  elif eltno == 22: name = 'NGON'; nnodes = 1
-  elif eltno == 23: name = 'NFACE'; nnodes = 1
-  return name, nnodes
-
 # Donne la dimension d'un element a partir de son no
-#def dimFromEltNo(eltno):
-#  if eltno == 2: return 0 # NODE
-#  elif eltno >= 3 and eltno <= 4: return 1 # BAR
-#  elif eltno >= 5 and eltno <= 6: return 2 # TRI
-#  elif eltno >= 7 and eltno <= 9: return 2 # QUAD
-#  elif eltno >=25 and eltno <= 28: return 2 #TRI et QUAD d'ordre 3
-#  else: return 3
+def eltNo2Dim(eltno):
+  if eltno in ELTNO2ELTINFO: dim = ELTNO2ELTINFO[eltno][2]
+  else:
+    dim = 3
+    print(f"Warning: eltNo2Dim: Element '{eltno}' not supported.")
+  return dim
+
+# Return (Element name, number of nodes, dimensionality) given an Element number
+def eltNo2EltInfo(eltno):
+  if eltno in ELTNO2ELTINFO:
+    name, nnodes, dim = ELTNO2ELTINFO[eltno]
+  else:
+    name, nnodes, dim = 'UNKNOWN', -1, 3
+    print(f"Warning: eltNo2EltInfo: Element '{eltno}' not supported.")
+  return name, nnodes, dim
 
 # -- Convertit un PointRange (pyTree) en indices de fenetres (Converter)
 def range2Window(r):
@@ -2842,10 +2841,9 @@ def clearList(list):
     if i != []: t.append(i)
   return t
 
-# -- Change a var name to a CGNS name
+# -- Change a variable name to a CGNS name
 def getCGNSName(v):
-  if v in name2CGNS: return name2CGNS[v]
-  else: return v
+  return VARNAME2CGNS.get(v, v)
 
 #==============================================================================
 # -- Conversion Arrays / Nodes --
@@ -3673,8 +3671,7 @@ def getZoneDim(zone):
         if lc == 0: return [gtype, np, ne, 'UNKNOWN', 3]
         elif lc == 1:
           elt = c[0][1][0]
-          eltName,stype = eltNo2EltName(elt)
-          cellDim = DIMELTS[elt]
+          eltName, _, cellDim = eltNo2EltInfo(elt)
           if elt == 22 or elt == 23: # NGON or NFACE
             eltName = 'NGON'
             data = getNodeFromName1(c[0], 'ElementStartOffset')
@@ -3712,7 +3709,7 @@ def getZoneDim(zone):
             return [gtype, np, ne, 'NGON', cellDim]
           else: # BE/ME
             cellDim = 0
-            for i in c: cellDim = max(cellDim, DIMELTS[i[1][0]])
+            for i in c: cellDim = max(cellDim, eltNo2Dim(i[1][0]))
             eltNames = dict.fromkeys(eltNames, None)
             return [gtype, np, ne, ','.join(name for name in eltNames), cellDim]
       else:
@@ -4227,7 +4224,7 @@ def setElementConnectivity2(z, array):
 def _setElementDim(z):
   GEl = getNodesFromType1(z, 'Elements_t')
   if GEl == []: return []
-  dimElt = [DIMELTS[i[1][0]] for i in GEl]
+  dimElt = [eltNo2Dim(i[1][0]) for i in GEl]
   maxdim = max(dimElt)
   for c, GE in enumerate(GEl):
     if dimElt[c] == maxdim: GE[1][1] = 0
@@ -4240,10 +4237,11 @@ def getElementNodes(z):
   GEl = getNodesFromType1(z, 'Elements_t')
   for GE in GEl:
     elt = GE[1][0]
-    if DIMELTS[elt] == dim: out.append(GE)
-    elif DIMELTS[elt] > dim:
+    dimElt = eltNo2Dim(elt)
+    if dimElt == dim: out.append(GE)
+    elif dimElt > dim:
       out.clear()
-      dim = DIMELTS[elt]
+      dim = dimElt
       out.append(GE)
   return out
 

Cassiopee/Converter/Converter/PyTree.py
@@ -1692,10 +1692,8 @@ def setFields(arrays, t, loc, writeDim=True):
         p = 0
         for v in vars:
             renamed = 0 # si le nom est change en nom CGNS = 1
-            if v in Internal.name2CGNS:
-                variable = Internal.name2CGNS[v]
-                if variable != v: renamed = 1
-            else: variable = v
+            variable = Internal.getCGNSName(v)
+            if variable != v: renamed = 1
             if (variable == 'CoordinateX' or variable == 'CoordinateY'
                     or variable == 'CoordinateZ') and loc == 'nodes':
                 coordNode = Internal.getNodeFromName1(z, Internal.__GridCoordinates__)
@@ -7825,7 +7823,7 @@ def breakConnectivity(t):
                     _deleteGridConnectivity__(zp)
                     GEl = Internal.getNodesFromType1(zp, 'Elements_t')
                     GE = Internal.getNodeFromName1(zp, connects[i][0])
-                    eltType, nf = Internal.eltNo2EltName(GE[1][0])
+                    eltType, _ = Internal.eltNo2EltName(GE[1][0])
                     # Nouveau nom de la zone
                     zp[0] = getZoneName(z[0]+'_'+eltType)
                     # Enleve toutes les connects volumiques a part la ieme

Cassiopee/KCore/KCore/Connect/connectNG2FE.cpp
@@ -34,28 +34,23 @@ using namespace K_FLD;
 //=============================================================================
 void K_CONNECT::connectNG2FE(FldArrayI& cNG, FldArrayI& cFE)
 {
-  // Acces non universel sur le ptrs
-  E_Int* nface = cNG.getNFace();
-  E_Int* indPH = cNG.getIndPH();
-  // Acces universel nbres de faces et d'elements
-  E_Int nfaces = cNG.getNFaces();
-  E_Int nelts = cNG.getNElts();
+  E_Int* nface = cNG.getNFace(); E_Int* indPH = cNG.getIndPH();
+  E_Int nfaces = cNG.getNFaces(); E_Int nelts = cNG.getNElts();
 
   // Connectivite faces->elts
   cFE.malloc(nfaces, 2); cFE.setAllValuesAtNull();
   E_Int* facesp1 = cFE.begin(1);
   E_Int* facesp2 = cFE.begin(2);
 
-  E_Int nf, face;
+  E_Int nf, fidx;
   for (E_Int i = 0; i < nelts; i++)
   {
-    // Acces universel element et
     E_Int* elt = cNG.getElt(i, nf, nface, indPH);
     for (E_Int j = 0; j < nf; j++)
     {
-      face = std::abs(elt[j])-1;
-      if (facesp1[face] == 0) facesp1[face] = i+1;
-      else facesp2[face] = i+1;
+      fidx = K_FUNC::E_abs(elt[j]) - 1;
+      if (facesp1[fidx] == 0) facesp1[fidx] = i+1;
+      else facesp2[fidx] = i+1;
     }
   }
 }

commit fb4c0c478c364a45d7908b696d12a35612207e25

Converter: adaptPE2NFace: fix error introduced in 06eba8a

Cassiopee/Converter/Converter/Adapter/adaptNFace2PE.cpp
@@ -95,7 +95,7 @@ PyObject* K_CONVERTER::adaptNFace2PE(PyObject* self, PyObject* args)
   PyObject* tpl = K_NUMPY::buildNumpyArray(nfaces, 2, 1, 1);
   E_Int* cFE = K_NUMPY::getNumpyPtrI(tpl);
 
-  if (cNGon->begin()[0] > 2 && methodPE != 0)
+  if (cNGon->begin()[0] <= 2 && methodPE != 0)
   {
     printf("adaptNFace2PE: NGon connectivity is not 3D, defaulting to method 0 "
            "(geometric) in which all elements are assumed to be "