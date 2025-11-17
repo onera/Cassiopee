b65fb425e0974d83375ad120a2778ff6af859d42
antjost <antoinejost.belmonte@gmail.com>
2025-11-17 19:15:59 +0100

    [TEST] bug fix for test case

--- Cassiopee/Geom/test/profilePT_t1.py
+++ Cassiopee/Geom/test/profilePT_t1.py
@@ -3,7 +3,9 @@ import Geom.PyTree as D
 import Converter.PyTree as C
 import KCore.test as test
 
+LOCAL = test.getLocal()
+
 # get one profile
 a = D.profile("SIKORSKY/SIKORSKYSSC-A07AIRFOIL")
-C.convertPyTree2File(a, 'out.cgns')
+#C.convertPyTree2File(a, LOCAL+'/out.cgns')
 test.testT(a, 1)
