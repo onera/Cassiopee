f1fdfc5315b0381015d3577ce27630cd9f47c48d
vincentcasseau <hystrath@gmail.com>
2025-11-30 19:24:10 +0100

    Correct test case

--- Cassiopee/Generator/test/getVolumeMapPT_t1.py
+++ Cassiopee/Generator/test/getVolumeMapPT_t1.py
@@ -2,7 +2,7 @@
 import Generator.PyTree as G
 import Converter.PyTree as C
 import Geom.PyTree as D
-import KCore.test as T
+import KCore.test as test
 
 # --- Structured grids ---
 # 3D structured
@@ -13,19 +13,19 @@ a = C.addBC2Zone(a, 'match2','BCMatch','imax',a,'imin',[1,2,3])
 a = C.fillEmptyBCWith(a,'overlap','BCOverlap')
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 1)
+test.testT(vol, 1)
 
 # 2D structured
 a = G.cart((0.,0.,0.), (0.1,0.1,0.2), (10,10,1))
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 2)
+test.testT(vol, 2)
 
 # 1D structured
 a = G.cart((0.,0.,0.), (0.1,0.1,0.2), (1,1,10))
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 7)
+test.testT(vol, 7)
 
 # --- BE ---
 # 1d unstructured bar
@@ -33,41 +33,41 @@ a = D.line((0,0,0), (1,0,0),11)
 a2 = C.convertArray2Tetra(a)
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 8)
+test.testT(vol, 8)
 
 # 2D unstructured quad
 a = G.cartHexa((0.,0.,0.), (0.1,0.1,0.2), (10,10,1))
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 3)
+test.testT(vol, 3)
 
 # 3D unstructured hexa
 a = G.cartHexa((0.,0.,0.), (0.1,0.1,0.2), (10,10,10))
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 4)
+test.testT(vol, 4)
 
 # 2D unstructured tri
 a = G.cartTetra((0.,0.,0.), (0.1,0.1,0.2), (10,10,1))
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 5)
+test.testT(vol, 5)
 
 # 3D unstructured tetra
 a = G.cartTetra((0.,0.,0.), (0.1,0.1,0.2), (10,10,10))
 a = C.initVars(a,'Density',1.); a = C.initVars(a,'centers:cellN',1.)
 vol = G.getVolumeMap(a)
-T.testT(vol, 6)
+test.testT(vol, 6)
 
 # 3D unstructured pyra
 a = G.cartPyra((0., 0., 0.), (1., 1., 1.), (5, 5, 5))
 G._getVolumeMap(a)
-T.testT(t,9)
+test.testT(a,9)
 
 # 3D unstructured penta
 a = G.cartPenta((0., 0., 0.), (1., 1., 1.), (5, 5, 5))
 G._getVolumeMap(a)
-T.testT(t,10)
+test.testT(a,10)
 
 """
 # 1D unstructured ME
@@ -78,7 +78,7 @@ d = G.cartHexa((0.4,-0.9,0.), (0.1,0.1,0.2), (1,1,12))
 a = C.mergeConnectivity([a, b, c, d], None)
 #G._getVolumeMap(a)
 C.convertPyTree2File(a, 'coucou1.cgns')
-#T.testT(t,11)
+#test.testT(t,11)
 
 # --- ME ---
 # 2D unstructured ME: tri-quad-tri
@@ -91,7 +91,7 @@ d = G.cartTetra((0.4,-0.9,0.), (0.1,0.1,0.2), (5,10,1))
 a = C.mergeConnectivity([a, b, c, d], None)
 #G._getVolumeMap(a)
 C.convertPyTree2File(a, 'coucou2.cgns')
-#T.testT(t,12)
+#test.testT(t,12)
 
 # 3D unstructured ME: tetra - pyra
 #                       |      |
@@ -103,5 +103,5 @@ d = G.cartHexa((0.4,0.,0.), (0.1,0.1,0.1), (5,5,5))
 a = C.mergeConnectivity([a, b, c, d], None)
 #G._getVolumeMap(a)
 C.convertPyTree2File(a, 'coucou3.cgns')
-#T.testT(t,13)
+#test.testT(t,13)
 """
