a15eac1327c975a80b16b356907f188d9118aad7
vincentcasseau <hystrath@gmail.com>
2025-12-03 15:30:17 +0100

    All: minor, correct import of KCore.test

--- Cassiopee/Post/test/computeGrad_t2.py
+++ Cassiopee/Post/test/computeGrad_t2.py
@@ -2,7 +2,7 @@
 import Converter as C
 import Post as P
 import Generator as G
-import KCore.test as T
+import KCore.test as test
 
 
 # cas 3D hexa
@@ -10,39 +10,39 @@ ni = 30; nj = 40; nk = 3
 m = G.cartHexa((0,0,0), (10./(ni-1),10./(nj-1),1), (ni,nj,nk))
 m = C.initVars(m, '{ro}=2*{x}+{x}*{y}')
 p = P.computeGrad(m, 'ro')
-T.testA([p], 1)
+test.testA([p], 1)
 
 # cas 3D tetra
 ni = 30; nj = 40; nk = 3
 m = G.cartTetra((0,0,0), (10./(ni-1),10./(nj-1),1), (ni,nj,nk))
 m = C.initVars(m, '{ro}=2*{x}+{x}*{y}')
 p = P.computeGrad(m, 'ro')
-T.testA([p], 2)
+test.testA([p], 2)
 #
 # cas 3D penta
 ni = 30; nj = 40; nk = 3
 m = G.cartPenta((0,0,0), (10./(ni-1),10./(nj-1),1), (ni,nj,nk))
 m = C.initVars(m, '{ro}=2*{x}+{x}*{y}')
 p = P.computeGrad(m, 'ro')
-T.testA([p], 3)
+test.testA([p], 3)
 #
 # cas 2D QUAD
 ni = 30; nj = 40; nk = 1
 m = G.cartHexa((0,0,0), (10./(ni-1),10./(nj-1),1), (ni,nj,nk))
 m = C.initVars(m, '{ro}=2*{x}+{x}*{y}')
 p = P.computeGrad(m, 'ro')
-T.testA([p], 4)
+test.testA([p], 4)
 
 # cas 2D tri
 ni = 30; nj = 40; nk = 1
 m = G.cartTetra((0,0,0), (10./(ni-1),10./(nj-1),1), (ni,nj,nk))
 m = C.initVars(m, '{ro}=2*{x}+{x}*{y}')
 p = P.computeGrad(m, 'ro')
-T.testA([p], 5)
+test.testA([p], 5)
 
 # cas 3D NGON
 ni = 30; nj = 40; nk = 3
 m = G.cartNGon((0,0,0), (10./(ni-1),10./(nj-1),1), (ni,nj,nk))
 m = C.initVars(m, '{ro}=2*{x}+{x}*{y}')
 p = P.computeGrad(m, 'ro')
-T.testA([p], 6)
+test.testA([p], 6)
