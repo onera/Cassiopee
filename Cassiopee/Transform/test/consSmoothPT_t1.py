a226a0359532868062a600b057cc3857dc932afb
fpelerin <francesca.pelerin@gmail.com>
2026-03-20 09:56:55 +0100

    Transform: consSmooth en Bar

--- Cassiopee/Transform/test/consSmoothPT_t1.py
+++ Cassiopee/Transform/test/consSmoothPT_t1.py
@@ -2,6 +2,7 @@
 import Transform.PyTree as T
 import Geom.PyTree as D
 import KCore.test as test
+import Converter.PyTree as C
 
 sweeps = 5
 l1 = D.line((0.,0.,0.), (0.,1.,0.), N=5)
@@ -18,3 +19,13 @@ test.testT(a, 1)
 b = T.join([l1,l2,l3])
 b = T.consSmooth(b,sweeps)
 test.testT(b, 2)
+
+# standard bar1D tests on closed curve
+c = C.convertArray2Tetra(a) 
+c = T.consSmooth(c,sweeps)
+test.testA([c], 3)
+
+# standard bar1D tests on open curve
+d = C.convertArray2Tetra(b) 
+d = T.consSmooth(d,sweeps)
+test.testA([d], 4)
--- Cassiopee/Transform/test/consSmooth_t1.py
+++ Cassiopee/Transform/test/consSmooth_t1.py
@@ -2,6 +2,7 @@
 import Transform as T
 import Geom as D
 import KCore.test as test
+import Converter as C
 
 sweeps = 5
 l1 = D.line((0.,0.,0.), (0.,1.,0.), N=5)
@@ -17,4 +18,15 @@ test.testA([a], 1)
 # standard array1 tests on open curve
 b = T.join([l1,l2,l3])
 b = T.consSmooth(b,sweeps)
+C.convertArrays2File(b, 'out.plt')
 test.testA([b], 2)
+
+# standard bar1D tests on closed curve
+c = C.convertArray2Tetra(a) 
+c = T.consSmooth(c,sweeps)
+test.testA([c], 3)
+
+# standard bar1D tests on open curve
+d = C.convertArray2Tetra(b) 
+d = T.consSmooth(d,sweeps)
+test.testA([d], 4)
