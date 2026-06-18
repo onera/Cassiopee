d2aaffa4a344fc49e457717e3381d51934438dd1
Vince <hystrath@gmail.com>
2026-06-18 07:58:25 +0200

    All: fix module import names (#672)

--- Cassiopee/Converter/test/getValue_t3.py
+++ Cassiopee/Converter/test/getValue_t3.py
@@ -1,12 +1,12 @@
 # - getValue (array) -
-import Converter as C
-import Generator.PyTree as GP
-import Converter.PyTree as CP
+import Converter
+import Generator.PyTree as G
+import Converter.PyTree as C
 import KCore.test as test
 
 # test on array3
-a = GP.cart((0,0,0), (1,1,1), (10,10,10))
-f = CP.getFields('GridCoordinates', a, api=3)[0]
-ret = C.getValue(f, 12)
+a = G.cart((0,0,0), (1,1,1), (10,10,10))
+f = C.getFields('GridCoordinates', a, api=3)[0]
+ret = Converter.getValue(f, 12)
 #C.setValue(f, 12, ret)
 test.testO(ret, 1)
--- Cassiopee/Converter/test/setValue_t3.py
+++ Cassiopee/Converter/test/setValue_t3.py
@@ -1,12 +1,12 @@
 # - setValue (array) -
-import Converter as C
-import Generator.PyTree as GP
-import Converter.PyTree as CP
+import Converter
+import Generator.PyTree as G
+import Converter.PyTree as C
 import KCore.test as test
 
 # test on array3
-a = GP.cart((0,0,0), (1,1,1), (10,10,10))
-f = CP.getFields('GridCoordinates', a, api=3)[0]
-C.setValue(f, 12, (1,2,3))
-ret = C.getValue(f, 12)
+a = G.cart((0,0,0), (1,1,1), (10,10,10))
+f = C.getFields('GridCoordinates', a, api=3)[0]
+Converter.setValue(f, 12, (1,2,3))
+ret = Converter.getValue(f, 12)
 test.testO(ret, 1)
