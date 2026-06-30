d5323c2b6e8dcf69c1a421e13aac3a72dbddf1f4
BconstantMMK <benjamin_constant@icloud.com>
2026-06-30 09:30:56 +0200

    KCore: name fix for compMinDist in compGeom

--- Cassiopee/KCore/KCore/CompGeom/compGeom.cpp
+++ Cassiopee/KCore/KCore/CompGeom/compGeom.cpp
@@ -208,7 +208,7 @@ void K_COMPGEOM::rectifyNormals(const E_Int ni1, const E_Int nj1, const E_Int in
 // Computes the minimum squared distance between 2 blocks and return
 // the corresponding cell indices
 // ============================================================================
-void K_COMPGEOM::compMeanDist(const E_Int ni1, const E_Int nj1,
+void K_COMPGEOM::compMinDist(const E_Int ni1, const E_Int nj1,
   const E_Float* x1, const E_Float* y1, const E_Float* z1,
   const E_Int ni2, const E_Int nj2,
   const E_Float* x2, const E_Float* y2, const E_Float* z2,
