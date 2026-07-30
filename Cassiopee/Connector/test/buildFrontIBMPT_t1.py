1f96da927d999704a16faad5599ac374a16b2e64
BconstantMMK <benjamin_constant@icloud.com>
2026-07-30 16:39:26 +0200

    X_IBM: update function headers and global cleaning. Remove unused correctionMulticorpsF42 option.

--- Cassiopee/Connector/test/buildFrontIBMPT_t1.py
+++ Cassiopee/Connector/test/buildFrontIBMPT_t1.py
@@ -34,31 +34,15 @@ a = G.cart((-1,-1,0), (h,h,1), (2*N,2*N,1)); a[0] = 'cart'
 t = C.newPyTree(['CART', Internal.getZones(a)])
 C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
 
-XIBM._dist2wallIBM(t, tb, dimPb=2, frontType=42)
+XIBM._dist2wallIBM(t, tb, dimPb=2)
 XIBM._blankingIBM(t, tb, dimPb=2, frontType=42)
 tc = C.node2Center(t)
 t, tc, front, front2, frontWMM  = XIBM.buildFrontIBM(t, tc, dimPb=2)
 test.testT(t, 2)
 test.testT(tc, 21)
 
-# --- Front Type=42, correction multicorps = True
-N = 50; h = 1./(N-1)
-c1 = G.cylinder((-0.5,-0.5,0.), 0.2, 1., 360., 0., 10., (2*N,1,1)); c1[0] = 'cyl1'
-c2 = G.cylinder((0.5,0.5,0.), 0.2, 1., 360., 0., 10., (2*N,1,1)); c2[0] = 'cyl2'
-tb = C.newPyTree(['CYL1',Internal.getZones(c1),
-                  'CYL2',Internal.getZones(c2)])
-DIBM._setIBCType(tb, 'Musker')
-
-a = G.cart((-1,-1,0), (h,h,1), (2*N,2*N,1)); a[0] = 'cart'
-t = C.newPyTree(['CART', Internal.getZones(a)])
-C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
-
-XIBM._dist2wallIBM(t, tb, dimPb=2, correctionMultiCorpsF42=True, frontType=42)
-XIBM._blankingIBM(t, tb, dimPb=2, correctionMultiCorpsF42=True, frontType=42)
-tc = C.node2Center(t)
-t, tc, front, front2, frontWMM  = XIBM.buildFrontIBM(t, tc, dimPb=2)
-test.testT(t, 3)
-test.testT(tc, 31)
+# following line is to avoid regression (test 3 removed)
+for i in range(4): _ = C.getBCName('farfield%d'%(i+1))
 
 # --- Front Type=42, blankingF42=True
 N = 50; h = 1./(N-1)
@@ -70,7 +54,7 @@ a = G.cart((-1,-1,0), (h,h,1), (2*N,2*N,1)); a[0] = 'cart'
 t = C.newPyTree(['CART', Internal.getZones(a)])
 C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
 
-XIBM._dist2wallIBM(t, tb, dimPb=2, frontType=42)
+XIBM._dist2wallIBM(t, tb, dimPb=2)
 XIBM._blankingIBM(t, tb, dimPb=2, frontType=42, blankingF42=True)
 tc = C.node2Center(t)
 t, tc, front, front2, frontWMM  = XIBM.buildFrontIBM(t, tc, dimPb=2)
@@ -87,7 +71,7 @@ a = G.cart((-1,-1,0), (h,h,1), (2*N,2*N,1)); a[0] = 'cart'
 t = C.newPyTree(['CART', Internal.getZones(a)])
 C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
 
-XIBM._dist2wallIBM(t, tb, dimPb=2, frontType=42)
+XIBM._dist2wallIBM(t, tb, dimPb=2)
 XIBM._blankingIBM(t, tb, dimPb=2, frontType=42, twoFronts=True)
 tc = C.node2Center(t)
 t, tc, front, front2, frontWMM  = XIBM.buildFrontIBM(t, tc, dimPb=2, twoFronts=True)
--- Cassiopee/Connector/test/setInterpDataIBMPT_t1.py
+++ Cassiopee/Connector/test/setInterpDataIBMPT_t1.py
@@ -25,7 +25,7 @@ XIBM._dist2wallIBM(t, tb, dimPb=2)
 XIBM._blankingIBM(t, tb, dimPb=2)
 tc = C.node2Center(t)
 t, tc, front, front2, frontWMM = XIBM.buildFrontIBM(t, tc, dimPb=2, cartesian=False)
-XIBM._setInterpDataIBM(t, tc, tb, front, dimPb=2, frontType=1, cartesian=False)
+XIBM._setInterpDataIBM(t, tc, tb, front=front, dimPb=2, frontType=1, cartesian=False)
 ####
 # The following lines are to avoid regression since the bug fix for duplicate information in tc
 ####
@@ -46,11 +46,11 @@ T._addkplane(t)
 T._contract(t, (0,0,0), (1,0,0), (0,1,0), 0.01)
 C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
 
-XIBM._dist2wallIBM(t, tb, dimPb=2, frontType=42)
+XIBM._dist2wallIBM(t, tb, dimPb=2)
 XIBM._blankingIBM(t, tb, dimPb=2, frontType=42)
 tc = C.node2Center(t)
 t, tc, front, front2, frontWMM = XIBM.buildFrontIBM(t, tc, dimPb=2, frontType=42, cartesian=False)
-XIBM._setInterpDataIBM(t, tc, tb, front, dimPb=2, frontType=42, cartesian=False)
+XIBM._setInterpDataIBM(t, tc, tb, front=front, dimPb=2, frontType=42, cartesian=False)
 ####
 # The following lines are to avoid regression since the bug fix for duplicate information in tc
 ####
@@ -59,32 +59,8 @@ Internal._rmNodesByName(tc, '.Solver#Param')
 test.testT(t, 2)
 test.testT(tc, 21)
 
-# --- Front Type=42, correction multicorps=True
-N = 50; h = 1./(N-1)
-c1 = G.cylinder((-0.5,-0.5,0.), 0.2, 1., 360., 0., 10., (2*N,1,1)); c1[0] = 'cyl1'
-c2 = G.cylinder((0.5,0.5,0.), 0.2, 1., 360., 0., 10., (2*N,1,1)); c2[0] = 'cyl2'
-tb = C.newPyTree(['CYL1', Internal.getZones(c1),
-                  'CYL2', Internal.getZones(c2)])
-DIBM._setIBCType(tb, 'Musker')
-
-a = G.cart((-1,-1,0), (h,h,1), (2*N,2*N,1)); a[0] = 'cart'
-t = C.newPyTree(['CART', Internal.getZones(a)])
-T._addkplane(t)
-T._contract(t, (0,0,0), (1,0,0), (0,1,0), 0.01)
-C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
-
-XIBM._dist2wallIBM(t, tb, dimPb=2, correctionMultiCorpsF42=True, frontType=42)
-XIBM._blankingIBM(t, tb, dimPb=2, correctionMultiCorpsF42=True, frontType=42)
-tc = C.node2Center(t)
-t, tc, front, front2, frontWMM = XIBM.buildFrontIBM(t, tc, dimPb=2, frontType=42, cartesian=False)
-XIBM._setInterpDataIBM(t, tc, tb, front, front2=front2, dimPb=2, frontType=42, cartesian=False)
-####
-# The following lines are to avoid regression since the bug fix for duplicate information in tc
-####
-Internal._rmNodesByName(tc, '.Solver#Param')
-####
-test.testT(t, 3)
-test.testT(tc, 31)
+# following line is to avoid regression (test 3 removed)
+for i in range(6): _ = C.getBCName('farfield%d'%(i+1))
 
 # --- Front Type=42 , blankingF42=True
 N = 50; h = 1./(N-1)
@@ -98,11 +74,11 @@ T._addkplane(t)
 T._contract(t, (0,0,0), (1,0,0), (0,1,0), 0.01)
 C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
 
-XIBM._dist2wallIBM(t, tb, dimPb=2, frontType=42)
+XIBM._dist2wallIBM(t, tb, dimPb=2)
 XIBM._blankingIBM(t, tb, dimPb=2, frontType=42, blankingF42=True)
 tc = C.node2Center(t)
 t, tc, front, front2, frontWMM = XIBM.buildFrontIBM(t, tc, dimPb=2, cartesian=False)
-XIBM._setInterpDataIBM(t, tc, tb, front, dimPb=2, frontType=42, cartesian=False)
+XIBM._setInterpDataIBM(t, tc, tb, front=front, dimPb=2, frontType=42, cartesian=False)
 ####
 # The following lines are to avoid regression since the bug fix for duplicate information in tc
 ####
@@ -123,11 +99,11 @@ T._addkplane(t)
 T._contract(t, (0,0,0), (1,0,0), (0,1,0), 0.01)
 C._fillEmptyBCWith(t, 'farfield', 'BCFarfield')
 
-XIBM._dist2wallIBM(t, tb, dimPb=2, frontType=42)
+XIBM._dist2wallIBM(t, tb, dimPb=2)
 XIBM._blankingIBM(t, tb, dimPb=2, frontType=42, twoFronts=True)
 tc = C.node2Center(t)
 t, tc, front, front2, frontWMM = XIBM.buildFrontIBM(t, tc, dimPb=2, frontType=42, twoFronts=True, cartesian=False)
-XIBM._setInterpDataIBM(t, tc, tb, front, front2=front2, dimPb=2, frontType=42, twoFronts=True, cartesian=False)
+XIBM._setInterpDataIBM(t, tc, tb, front=front, front2=front2, dimPb=2, frontType=42, twoFronts=True, cartesian=False)
 ####
 # The following lines are to avoid regression since the bug fix for duplicate information in tc
 ####
