3e4f73f15743b1412c715b6a842629eac80136df
vincentcasseau <hystrath@gmail.com>
2026-09-01 11:45:45 +0200

    Post: add missing dim arg. to Xmpi.connectMatch

--- Cassiopee/Post/test/computeDiv2PT_m1.py
+++ Cassiopee/Post/test/computeDiv2PT_m1.py
@@ -33,14 +33,18 @@ def _createTest(filepath, meshType="STRUCT", dim=3, axis="X", api=3):
     if Cmpi.master:
         zones = []
         for rank in range(2):
-            xblhc = 0.
-            yblhc = float(rank)*(N-1)
-            zblhc = 0.
-            Nz = N if dim == 3 else 1
+            blhc = [
+                float(rank)*(N-1) if ax == axis.upper() else 0.
+                for ax in ["X", "Y", "Z"]
+            ]
+            if dim == 3: Nxyz = [N, N, N]; trhc = (1., 1., 1.)
+            else:
+                if axis.upper() == "Z": Nxyz = [N, 1, N]; trhc = (1., 0., 1.)
+                else: Nxyz = [N, N, 1]; trhc = (1., 1., 0.)
             if meshType.upper() == "NGON":
-                a = G.cartNGon((xblhc, yblhc, zblhc), (1., 1., 1.), (N, N, Nz), api=api)
+                a = G.cartNGon(blhc, trhc, Nxyz, api=api)
             elif meshType.upper() == "STRUCT":
-                a = G.cart((xblhc, yblhc, zblhc), (1., 1., 1.), (N, N, Nz))
+                a = G.cart(blhc, trhc, Nxyz)
             Cmpi._setProc(a, rank)
             zones.append(a)
         t = C.newPyTree(["Base", *zones])
@@ -48,7 +52,7 @@ def _createTest(filepath, meshType="STRUCT", dim=3, axis="X", api=3):
         C.convertPyTree2File(t, filepath)
     Cmpi.barrier()
 
-def runTest(filepath, meshType="STRUCT"):
+def runTest(filepath, meshType="STRUCT", dim=3):
     if meshType.upper() == "NGON":
         h = Filter.Handle(filepath)
         t = h.loadFromProc()
@@ -58,7 +62,7 @@ def runTest(filepath, meshType="STRUCT"):
     elif meshType.upper() == "STRUCT":
         h = Filter.Handle(filepath)
         t = h.loadFromProc()
-        t = Xmpi.connectMatch(t)
+        t = Xmpi.connectMatch(t, dim=dim)
         Pmpi._computeDiv2(t, var='centers:Velocity', rmVar=False)
     #Cmpi.convertPyTree2File(t, filepath)
     if Cmpi.master: os.remove(filepath)
@@ -71,54 +75,60 @@ filepath = os.path.join(LOCAL, "out.cgns")
 # --- Without BCDataSets --- #
 # 2D STRUCT
 meshType = "STRUCT"
-#_createTest(filepath, meshType, dim=2, axis="X")
-#t = runTest(filepath, meshType)
+dim = 2
+#_createTest(filepath, meshType, dim=dim, axis="X")
+#t = runTest(filepath, meshType, dim=dim)
 #if Cmpi.master: test.testT(t, 1) # CRASH!
 
-#_createTest(filepath, meshType, dim=2, axis="Y")
-#t = runTest(filepath, meshType)
+#_createTest(filepath, meshType, dim=dim, axis="Y")
+#t = runTest(filepath, meshType, dim=dim)
 #if Cmpi.master: test.testT(t, 2) # CRASH!
 
-#_createTest(filepath, meshType, dim=2, axis="Z")
-#t = runTest(filepath, meshType)
+#_createTest(filepath, meshType, dim=dim, axis="Z")
+#t = runTest(filepath, meshType, dim=dim)
 #if Cmpi.master: test.testT(t, 3) # CRASH!
 
 # 3D STRUCT
-_createTest(filepath, meshType, dim=3, axis="X")
-t = runTest(filepath, meshType)
+meshType = "STRUCT"
+dim = 3
+_createTest(filepath, meshType, dim=dim, axis="X")
+t = runTest(filepath, meshType, dim=dim)
 if Cmpi.master: test.testT(t, 4)
 
-_createTest(filepath, meshType, dim=3, axis="Y")
-t = runTest(filepath, meshType)
-if Cmpi.master: test.testT(t, 5) # WRONG REF!
+_createTest(filepath, meshType, dim=dim, axis="Y")
+t = runTest(filepath, meshType, dim=dim)
+if Cmpi.master: test.testT(t, 5)
 
-_createTest(filepath, meshType, dim=3, axis="Z")
-t = runTest(filepath, meshType)
+_createTest(filepath, meshType, dim=dim, axis="Z")
+t = runTest(filepath, meshType, dim=dim)
 if Cmpi.master: test.testT(t, 6)
 
 # 2D NGON
 meshType = "NGON"
-#_createTest(filepath, meshType, dim=2, axis="X", api=1)
-#t = runTest(filepath, meshType)
-#if Cmpi.master: test.testT(t, 7) # CRASH!
+dim = 2
+_createTest(filepath, meshType, dim=dim, axis="X", api=1)
+t = runTest(filepath, meshType, dim=dim)
+#if Cmpi.master: test.testT(t, 7) # WRONG REF!
 
-#_createTest(filepath, meshType, dim=2, axis="Y", api=3)
-#t = runTest(filepath, meshType)
-#if Cmpi.master: test.testT(t, 8) # CRASH!
+_createTest(filepath, meshType, dim=dim, axis="Y", api=3)
+t = runTest(filepath, meshType, dim=dim)
+#if Cmpi.master: test.testT(t, 8) # WRONG REF!
 
-#_createTest(filepath, meshType, dim=2, axis="Z", api=3)
-#t = runTest(filepath, meshType)
-#if Cmpi.master: test.testT(t, 9) # CRASH!
+_createTest(filepath, meshType, dim=dim, axis="Z", api=3)
+t = runTest(filepath, meshType, dim=dim)
+#if Cmpi.master: test.testT(t, 9) # WRONG REF!
 
 # 3D NGON
-_createTest(filepath, meshType, dim=3, axis="X", api=1)
-t = runTest(filepath, meshType)
+meshType = "NGON"
+dim = 3
+_createTest(filepath, meshType, dim=dim, axis="X", api=1)
+t = runTest(filepath, meshType, dim=dim)
 if Cmpi.master: test.testT(t, 10)
 
-_createTest(filepath, meshType, dim=3, axis="Y", api=3)
-t = runTest(filepath, meshType)
+_createTest(filepath, meshType, dim=dim, axis="Y", api=3)
+t = runTest(filepath, meshType, dim=dim)
 if Cmpi.master: test.testT(t, 11)
 
-_createTest(filepath, meshType, dim=3, axis="Z", api=3)
-t = runTest(filepath, meshType)
+_createTest(filepath, meshType, dim=dim, axis="Z", api=3)
+t = runTest(filepath, meshType, dim=dim)
 if Cmpi.master: test.testT(t, 12)
