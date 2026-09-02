06052bb6ccaff4d282f2c94776a0330b72e640a7
vincentcasseau <hystrath@gmail.com>
2026-09-02 13:22:19 +0200

    minor

--- Cassiopee/Post/test/computeGrad2PT_t1.py
+++ Cassiopee/Post/test/computeGrad2PT_t1.py
@@ -37,10 +37,21 @@ C._initVars(t,'{centers:Density}=2*{centers:CoordinateX}+{centers:CoordinateX}*{
 P._computeGrad2(t, 'centers:Density')
 test.testT(t, 0)
 
-# 2D NGON (z=0, nk=1)
-ni = 30; nj = 40
-t = G.cartNGon((0,0,0), (10./(ni-1),10./(nj-1),0), (ni,nj,1))
-C._initVars(t,'{centers:Density}={centers:CoordinateX}')
+# 2D NGON
+ni = 30; nj = 40; nk = 1
+t = G.cartNGon((0,0,0), (10./(ni-1),10./(nj-1),0), (ni,nj,nk))
+C._initVars(t,'{centers:Density}=3*{centers:CoordinateX}+2*{centers:CoordinateY}-{centers:CoordinateZ}')
 P._computeGrad2(t, 'centers:Density')
-C.convertPyTree2File(t, "out.cgns")
-#test.testT(t, 5)
+test.testT(t, 5)
+
+ni = 30; nj = 1; nk = 40
+t = G.cartNGon((0,0,0), (10./(ni-1),0,10./(nk-1)), (ni,nj,nk))
+C._initVars(t,'{centers:Density}=3*{centers:CoordinateX}+2*{centers:CoordinateY}-{centers:CoordinateZ}')
+P._computeGrad2(t, 'centers:Density')
+test.testT(t, 6)
+
+ni = 1; nj = 30; nk = 40
+t = G.cartNGon((0,0,0), (0,10./(nj-1),10./(nk-1)), (ni,nj,nk))
+C._initVars(t,'{centers:Density}=3*{centers:CoordinateX}+2*{centers:CoordinateY}-{centers:CoordinateZ}')
+P._computeGrad2(t, 'centers:Density')
+test.testT(t, 7)
