da9f15a008ccb8447dfb6d66e1ee7626091fc835
vincentcasseau <hystrath@gmail.com>
2026-09-04 16:32:38 +0200

    Converter: add test and doc for C.deleteEmptyBases

---
+++ Cassiopee/Converter/test/deleteEmptyBasesPT_t1.py
@@ -0,0 +1,11 @@
+# - deleteEmptyBases (pyTree) -
+import Converter.PyTree as C
+import Generator.PyTree as G
+import KCore.test as test
+
+a = G.cart((0,0,0), (1,1,1), (3,3,3))
+b = G.cart((2,0,0), (1,1,1), (3,3,3))
+
+t = C.newPyTree(['Base1', a, 'Base2', 'Base3', b])
+t = C.deleteEmptyBases(t)
+test.testT(t, 1)
