f10f078f4f3e5568db187db8affc5d382cafb26b
BconstantMMK <benjamin_constant@icloud.com>
2026-09-23 10:19:48 +0200

    Converter: center2Node NGON with BCDataset

--- Cassiopee/Converter/test/center2NodePT_m2.py
+++ Cassiopee/Converter/test/center2NodePT_m2.py
@@ -13,6 +13,7 @@ def F2(x,y,z): return x*y
 
 N = 5
 
+# center2Node NGON + BCMatch
 a = G.cartNGon((0.,0.,0.), (1./N,1./N,1./N), (N+1,N+1,N+1), api=3); a[0] = 'zoneA'
 b = T.translate(a, (1,0,0)); b[0] = 'zoneB'
 t = C.newPyTree(['Base', [a,b]])
@@ -34,3 +35,36 @@ t = Cmpi.center2Node(t, var=['centers:Density', 'centers:Density2'])
 Internal._rmNodesByName(t, Internal.__FlowSolutionCenters__)
 
 if Cmpi.master: test.testT(t, 1)
+
+# center2Node NGON + BCMatch + BCDataset
+a = G.cartNGon((0.,0.,0.), (1./N,1./N,1./N), (N+1,N+1,N+1), api=3); a[0] = 'zoneA'
+b = T.translate(a, (1,0,0)); b[0] = 'zoneB'
+
+bc1 = G.cartNGon((0.,0.,0.), (1,1./N,1./N), (1,N+1,N+1), api=3); bc1[0] = 'bc1'
+bc2 = T.translate(bc1, (2,0,0)); bc2[0] = 'bc2'
+
+C._addBC2Zone(a, 'inflow', 'BCInflow', subzone=bc1)
+C._addBC2Zone(b, 'outflow', 'BCOutflow', subzone=bc2)
+
+inflow = Internal.getNodeFromNameAndType(a, 'inflow', 'BC_t')
+C._initBCDataSet(inflow, 'Density', 2)
+
+outflow = Internal.getNodeFromNameAndType(b, 'outflow', 'BC_t')
+C._initBCDataSet(outflow, 'Density', -2)
+
+t = C.newPyTree(['Base', [a,b]])
+C._initVars(t, 'centers:Density', F1, ['centers:CoordinateX','centers:CoordinateY','centers:CoordinateZ'], isVectorized=True)
+
+for i, z in enumerate(Internal.getZones(t)):
+    Cmpi._setProc(z, i)
+
+# removes zones with proc != rank
+Cmpi._convert2PartialTree(t, Cmpi.rank)
+
+zones = Internal.getZones(t)
+Xmpi._connectMatchNGon(zones[0])
+
+t = Cmpi.center2Node(t, var=['centers:Density'])
+Internal._rmNodesByName(t, Internal.__FlowSolutionCenters__)
+
+if Cmpi.master: test.testT(t, 2)
